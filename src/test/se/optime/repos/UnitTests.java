3f473be4861976cccd0425a903615b66f23cf481
Staffan Olsson <staffan@repos.se>
2004-09-23 16:17:03 +0000

    mail accounts
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@28 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ src/test/se/optime/repos/UnitTests.java
@@ -0,0 +1,19 @@
+/*
+ * Created on Sep 23, 2004
+ */
+package se.optime.repos;
+
+import se.optime.repos.user.BasicAuthenticationManagerTest;
+import junit.framework.TestSuite;
+
+/**
+ * @author solsson
+ * @version $Id$
+ */
+public class UnitTests extends TestSuite {
+
+    public UnitTests() {
+        
+    }
+    
+}
