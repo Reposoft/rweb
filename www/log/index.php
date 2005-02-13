0e9d1969db5becc34b7c53b2f36c4d7b17c564f3
Staffan Olsson <staffan@repos.se>
2005-02-13 12:54:59 +0000

    This should work on the linux box.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@387 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/log/index.php
+++ www/log/index.php
@@ -7,8 +7,7 @@ require_once( upOne(dirname(__FILE__)) . "/login.inc.php" );
 define('STYLESHEET','../svnlayout/repos.xsl');
 
 $url = getTargetUrl();
-
-$cmd = getSvnCommand()." log $url";
+$cmd = getSvnCommand()." log --xml --incremental $url";
 
 // passthrough with stylesheet
 header('Content-type: text/xml');
