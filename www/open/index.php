54b93b0db9afb1e30fc27d7c4858e3d96c4b3645
Staffan Olsson <staffan@repos.se>
2006-04-14 06:43:44 +0000

    Default 'open' handling should be the same as clicking a link. Might be possible to separate open and download, but this has not been specified yet.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@1086 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/open/index.php
+++ www/open/index.php
@@ -14,22 +14,12 @@ $type = substr($url, strrpos($url, '.') + 1);
 
 // iCalendar files
 if ($type=='ics') {
-	/* PHP iCalendsr config must be changed to: 
-		$allow_webcals  = 'yes';
-		$allow_login = 'yes';
-	*/
-	header("Location: ../phpicalendar/?cal=$url");
+        /* PHP iCalendsr config must be changed to:
+                $allow_webcals  = 'yes';
+                $allow_login = 'yes';
+        */
+        header("Location: ../phpicalendar/?cal=$url");
 } else {
-?>
-<html>
-<head>
-<title>Open file in online editor</title>
-</head>
-<body>
-<p>Support for file type &quot;<?php echo $type; ?>&quot; not added yet.</p>
-<p>Click the file name to download and open the file</p>
-<p><a href="#" onclick="history.back()">Back</a></p>
-</body>
-<?php
+        header("Location: $url");
 }
 ?>
