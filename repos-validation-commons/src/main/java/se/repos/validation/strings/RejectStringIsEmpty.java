7ee591b4fdd12a89504a482019d8129283851352
Staffan Olsson <staffan@repos.se>
2006-06-11 12:17:32 +0000

    Tried to move package between components.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@1172 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ repos-validation-commons/src/main/java/se/repos/validation/strings/RejectStringIsEmpty.java
@@ -0,0 +1,33 @@
+/* Copyright 2005 Optime data Sweden
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.repos.validation.strings;
+
+import se.repos.text.InputUtils;
+import se.repos.validation.ValidationRejectStrategy;
+
+/**
+ * Rejects strings that are null, zero length or only space characters.
+ * @author Staffan Olsson
+ * @since 2005-nov-13
+ * @version $Id$
+ */
+public class RejectStringIsEmpty extends ValidationRejectStrategy<String> {
+
+	@Override
+	public boolean rejects(String value) {
+		return InputUtils.trim(value) == null;
+	}
+
+}
