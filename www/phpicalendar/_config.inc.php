af54ca7b994edde3a01882cf4a43e48b6c9d2d61
Staffan Olsson <staffan@repos.se>
2006-06-11 20:44:41 +0000

    Upgrading to phpicalendar 2.22
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@1177 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/phpicalendar/_config.inc.php
+++ www/phpicalendar/_config.inc.php
@@ -11,6 +11,7 @@ $minical_view 			= 'current';		// Where do the mini-calendars go when clicked? =
 $default_cal 			= $ALL_CALENDARS_COMBINED;		// Exact filename of calendar without .ics. Or set to $ALL_CALENDARS_COMBINED to open all calenders combined into one.
 $language 				= 'English';		// Language support - 'English', 'Polish', 'German', 'French', 'Dutch', 'Danish', 'Italian', 'Japanese', 'Norwegian', 'Spanish', 'Swedish', 'Portuguese', 'Catalan', 'Traditional_Chinese', 'Esperanto', 'Korean'
 $week_start_day 		= 'Sunday';			// Day of the week your week starts on
+$week_length			= '7';				// Number of days to display in the week view
 $day_start 				= '0700';			// Start time for day grid
 $day_end				= '2300';			// End time for day grid
 $gridLength 			= '15';				// Grid distance in minutes for day view, multiples of 15 preferred
@@ -19,18 +20,16 @@ $month_event_lines 		= '1';				// Number of lines to wrap each event title in mo
 $tomorrows_events_lines = '1';				// Number of lines to wrap each event title in the 'Tommorrow's events' box, 0 means display all lines.
 $allday_week_lines 		= '1';				// Number of lines to wrap each event title in all-day events in week view, 0 means display all lines.
 $week_events_lines 		= '1';				// Number of lines to wrap each event title in the 'Tommorrow's events' box, 0 means display all lines.
-$timezone 				= '';		// Set timezone. Read TIMEZONES file for more information
-$calendar_path 			= '';				// Leave this blank on most installs, place your full path to calendars if they are outside the phpicalendar folder.
+$timezone 				= '';				// Set timezone. Read TIMEZONES file for more information
+$calendar_path 			= '';				// Leave this blank on most installs, place your full FILE SYSTEM PATH to calendars if they are outside the phpicalendar folder.
 $second_offset			= '';				// The time in seconds between your time and your server's time.
-$bleed_time				= '';				// This allows events past midnight to just be displayed on the starting date, only good up to 24 hours. Range from '0000' to '2359', or '-1' for no bleed time. Is automatically set to $day_start if left blank.
+$bleed_time				= '-1';				// This allows events past midnight to just be displayed on the starting date, only good up to 24 hours. Range from '0000' to '2359', or '-1' for no bleed time.
 $cookie_uri				= ''; 				// The HTTP URL to the PHP iCalendar directory, ie. http://www.example.com/phpicalendar -- AUTO SETTING -- Only set if you are having cookie issues.
 $download_uri			= ''; 				// The HTTP URL to your calendars directory, ie. http://www.example.com/phpicalendar/calendars -- AUTO SETTING -- Only set if you are having subscribe issues.
-$default_path			= 'http://www.example.com/phpicalendar'; 						// The HTTP URL to the PHP iCalendar directory, ie. http://www.example.com/phpicalendar
+$default_path			= ''; 				// The HTTP URL to the PHP iCalendar directory, ie. http://www.example.com/phpicalendar
 $charset				= 'UTF-8';			// Character set your calendar is in, suggested UTF-8, or iso-8859-1 for most languages.
 
 // Yes/No questions --- 'yes' means Yes, anything else means no. 'yes' must be lowercase.
-$display_custom_goto 	= 'no';				// In the 'Jump To' box, display the custom 'go to day' box.
-$display_ical_list 		= 'yes';			// In the 'Jump To' box, display the pop-up menu with the list of all calendars in the $calendar_path directory.
 $allow_webcals 			= 'no';				// Allow http:// and webcal:// prefixed URLs to be used as the $cal for remote viewing of "subscribe-able" calendars. This does not have to be enabled to allow specific ones below.
 $this_months_events 	= 'yes';			// Display "This month's events" at the bottom off the month page.
 $enable_rss				= 'yes';			// Enable RSS access to your calendars (good thing).
@@ -41,28 +40,31 @@ $show_todos				= 'yes';			// Show your todo list on the side of day and week vie
 $show_completed			= 'yes';				// Show completed todos on your todo list.
 $allow_login			= 'no';				// Set to yes to prompt for login to unlock calendars.
 $login_cookies			= 'no';			// Set to yes to store authentication information via (unencrypted) cookies. Set to no to use sessions.
+$support_ical			= 'no';			// Set to yes to support the Apple iCal calendar database structure.
+$recursive_path			= 'no';			// Set to yes to recurse into subdirectories of the calendar path.
 
 // Calendar Caching (decreases page load times)
-$save_parsed_cals 		= 'no';				// Saves a copy of the cal in /tmp after it's been parsed. Improves performence.
-$tmp_dir				= '/tmp';			// The temporary directory on your system (/tmp is fine for UNIXes including Mac OS X). Any php-writable folder works.
+$save_parsed_cals 		= 'yes';				// Saves a copy of the cal in /tmp after it's been parsed. Improves performance.
+$tmp_dir				= '';			// The temporary directory on your system (/tmp is fine for UNIXes including Mac OS X). Any php-writable folder works.
 $webcal_hours			= '24';				// Number of hours to cache webcals. Setting to '0' will always re-parse webcals.
 
 // Webdav style publishing
 $phpicalendar_publishing = '';				// Set to '1' to enable remote webdav style publish. See 'calendars/publish.php' for complete information;
 
-// Administration settings (admin.php)
-$allow_admin			= 'yes';				// Set to yes to allow the admin page - remember to change the default password if using 'internal' as the $auth_method			
+// Administration settings (/admin/)
+$allow_admin			= 'yes';			// Set to yes to allow the admin page - remember to change the default password if using 'internal' as the $auth_method			
 $auth_method			= 'ftp';			// Valid values are: 'ftp', 'internal', or 'none'. 'ftp' uses the ftp server's username and password as well as ftp commands to delete and copy files. 'internal' uses $auth_internal_username and $auth_internal_password defined below - CHANGE the password. 'none' uses NO authentication - meant to be used with another form of authentication such as http basic.
 $auth_internal_username	= 'admin';			// Only used if $auth_method='internal'. The username for the administrator.
-$auth_internal_password	= 'admin';		// Only used if $auth_method='internal'. The password for the administrator.
+$auth_internal_password	= 'admin';			// Only used if $auth_method='internal'. The password for the administrator.
 $ftp_server				= 'localhost';		// Only used if $auth_method='ftp'. The ftp server name. 'localhost' will work for most servers.
+$ftp_port				= '21';				// Only used if $auth_method='ftp'. The ftp port. '21' is the default for ftp servers.
 $ftp_calendar_path		= '';				// Only used if $auth_method='ftp'. The full path to the calendar directory on the ftp server. If = '', will attempt to deduce the path based on $calendar_path, but may not be accurate depending on ftp server config.
 
 // Calendar colors
 //
 // You can increase the number of unique colors by adding additional images (monthdot_n.gif) 
 // and in the css file (default.css) classes .alldaybg_n, .eventbg_n and .eventbg2_n
-// Colors will rotate to double the number listed below. (That's 14 colors, 2 calendars per color).
+// Colors will repeat from the beginning for calendars past $unique_colors (7 by default), with no limit.
 $unique_colors			= '7';				
 
 $blacklisted_cals[] = '';					// Fill in between the quotes the name of the calendars 
@@ -74,8 +76,10 @@ $blacklisted_cals[] = '';					// the parser will *not* parse any cal that is in 
 $list_webcals[] = '';						// Fill in between the quotes exact URL of a calendar that you wish
 $list_webcals[] = '';						// to show up in your calendar list. You must prefix the URL with http://
 $list_webcals[] = '';						// or webcal:// and the filename should contain the .ics suffix
-$list_webcals[] = '';						// $allow_webcals does *not* need to be "yes" for these to show up and work
+#$list_webcals[] = '';						// $allow_webcals does *not* need to be "yes" for these to show up and work
 // add more lines as necessary
+
+#$more_webcals['cpath'][] = ''				//add webcals that will show up only for a particular cpath.
 
 $locked_cals[] = '';						// Fill in-between the quotes the names of the calendars you wish to hide
 $locked_cals[] = '';						// unless unlocked by a username/password login. This should be the
