11fc426da0f98417b25377c3bb2843724ff826f5
Staffan Olsson <staffan@repos.se>
2006-06-29 06:56:31 +0000

    Oops, copied wrong folder.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@1185 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-validation-annotations/repos-validation/src/main/java/se/repos/validation/ValidationStrategy.java
+++
@@ -1,9 +0,0 @@
-package se.repos.validation;
-
-public interface ValidationStrategy<V> {
-
-	ValidationResult ACCEPT = ValidationResult.VALID;
-	ValidationResult REJECT = ValidationResult.INVALID;
-	
-	public ValidationResult validate(V value);
-}
--- repos-validation-annotations/repos-validation/src/main/java/se/repos/validation/annotations/ValidationStrategyAnnotation.java
+++
@@ -1,5 +0,0 @@
-package se.repos.validation.annotations;
-
-public @interface ValidationStrategyAnnotation {
-
-}
