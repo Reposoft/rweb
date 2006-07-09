396cea6dd3530450ad6fc78d480e2ea47658efd0
Staffan Olsson <staffan@repos.se>
2006-07-09 10:03:55 +0000

    Some new ideas. EJB3 has a @Stateless annotation, so it might be worth investigating.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@1210 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ repos-object/src/main/java/se/repos/object/Stateless.java
@@ -0,0 +1,11 @@
+package se.repos.object;
+
+/**
+ * Immutable of course, but does not contain any data.
+ * @author solsson
+ * @since 2006 jul 9
+ * @version $Id$
+ */
+public @interface Stateless {
+
+}
