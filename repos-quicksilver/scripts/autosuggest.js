0066465ce5d6c28914d44773ab5c05bc15577ad0
Staffan Olsson <staffan@repos.se>
2006-08-14 10:19:40 +0000

    Now ESC works (hides quay)
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@1285 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-quicksilver/scripts/autosuggest.js
+++ repos-quicksilver/scripts/autosuggest.js
@@ -19,6 +19,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 *******************************************************
 /** modified for repos.se by Staffan och Kalle **/
+/** This file is still LGPL **/
 
 /********************************************************
  The AutoSuggest class binds to a text input field
