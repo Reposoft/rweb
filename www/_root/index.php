08f15f0ba9eb0f74ec44ec6199d10f3adc495458
Staffan Olsson <staffan@repos.se>
2006-08-21 12:37:00 +0000

    Clarified a bit. Tested logout in firefox and IE. Seems that firefox falls back to the 'void' logout strategy.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@1387 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/_root/index.php
+++ www/_root/index.php
@@ -17,10 +17,18 @@ header("Pragma: no-cache");
 
 // ------- hard coded settings -------
 // Server startpage, browser will be redirected here when logout is done / not needed
-$startpage = 'http://www.repos.se/repos/';
+function getAfterLogoutPage() {
+	return 'http://www.repos.se/repos/';
+}
 // Realm to send to verify login befor logout
 // maybe this should be retreived from session or from repos.properties
-$realm = 'Optime';
+function getRealm() {
+	return 'Optime';
+}
+
+function getVerifyPage() {
+	return '?login=verify';
+}
 
 // first the logout procedure on ?logout=
 if (isset($_GET['logout'])) {
@@ -32,7 +40,7 @@ if (isset($_GET['logout'])) {
 				showCouldNotLogOutPage();
 			}
 		} else {
-			showStartPage();
+			showAfterLogoutPage();
 		}
 	} else {
 		if (isset($_SERVER['PHP_AUTH_USER'])) {
@@ -41,64 +49,47 @@ if (isset($_GET['logout'])) {
 			requireAuth();
 		}
 	}
-	
-	
 } else {
-	showStartPage();
+	showAfterLogoutPage();
 }
 
-// go to home page
-function showStartPage() {
-	header('Location: '.$startpage);
+function showAfterLogoutPage() {
+	header('Location: '.getAfterLogoutPage());
 }
 
 function doLogout() {
-	header('HTTP/1.0 401 Unauthorized');
+	header('HTTP/1.1 401 Unauthorized');
 	showLoggingOutPage();
 }
 
 function doLogoutVoid() {
-	header('HTTP/1.0 401 Unauthorized');
-	showStartPage();
+	header('HTTP/1.1 401 Unauthorized');
+	showAfterLogoutPage();
 }
 
 function requireAuth() {
-	header('WWW-Authenticate: Basic realm="' . $realm . '"');
+	header('WWW-Authenticate: Basic realm="' . getRealm() . '"');
 	header('HTTP/1.1 401 Authorization Required');
 }
 
 // --- logout pages ---
 
 function showLoggingOutPage() {
+	$next = getVerifyPage();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <meta http-equiv="refresh" content="3;url=?logout=verify">
-<title>Logging out of repos.se</title>
+<title>Logging out of repos.se ...</title>
 </head>
-<p>Logging out of repos.se<p>
-<p><small>You should be automatically redirected to the <a href="?logout=verify">startpage</a>.</small></p>
+<p>Logging out of repos.se ...<p>
+<p><small>If your browser does not redirect automatically, click <a href="<?php echo($next); ?>">here</a>.</small></p>
 <body>
 </body>
 </html>
 <?php
-}
-
-function showLoggedOutPage() {
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Logged out of repos.se</title>
-</head>
-
-<body>
-</body>
-</html>
-<?php 
 }
 
 function showCouldNotLogOutPage() {
@@ -112,7 +103,7 @@ function showCouldNotLogOutPage() {
 </head>
 <script language="javascript">
 document.execCommand('ClearAuthenticationCache') //clear cache
-parent.location.href="../../" //redirect after logged out
+parent.location.href="<?php echo(getAfterLogoutPage()); ?>" //redirect after logged out
 </script>
 <body>
 <p>Clearing your browser's credentials for repos.se.</p>
@@ -122,8 +113,9 @@ parent.location.href="../../" //redirect after logged out
 	} else {
 		$logout_url = 'http';
 		if($_SERVER['SERVER_PORT']==443) $logout_url = $logout_url.'s';
-		$logout_url = $logout_url . "://void:LoggedOut@" . $_SERVER['SERVER_NAME'] .  ':' . $_SERVER['SERVER_PORT'] . $_SERVER['SCRIPT_NAME'] . '?logout=verify';
+		$logout_url = $logout_url . "://void:LoggedOut@" . $_SERVER['SERVER_NAME'] .  ':' . $_SERVER['SERVER_PORT'] . '/' . getVerifyPage();
 		// note that redirect including a password is illegal in IE
 		header("Location: $logout_url");
 	}
 }
+?>
