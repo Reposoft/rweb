68d8601356c0642ad8659a9ac8243734250e60ca
Staffan Olsson <staffan@repos.se>
2006-08-29 14:17:35 +0000

    new files
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@1444 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ www/view/index.php
@@ -0,0 +1,12 @@
+<?php
+require(dirname(dirname(__FILE__)) . '/conf/repos.properties.php');
+
+if(isset($_GET['result'])) {
+	$resultFile = getTempDir('pages') . $_GET['result'];
+	$handle = fopen($resultFile, "r");
+	fpassthru($handle);
+	fclose($handle);
+	exit;
+} else {
+	trigger_error('Result ID not set. Can not view page.');
+}
