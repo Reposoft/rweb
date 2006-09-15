a6e91387bae0bb66bcb87beac97f00edeb63c806
Staffan Olsson <staffan@repos.se>
2006-09-15 15:00:13 +0000

    Work in progress on the tree
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@1621 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/open/tree/RepositoryTree.class.php
+++ www/open/tree/RepositoryTree.class.php
@@ -1,10 +1,99 @@
 <?php
-class RepositoryTree {
 
-	function ReposiotyrTree($aclFile, $username) {
+/**
+ * Represents the starting poins a user has in a repository.
+ */
+class RepositoryTree {
+	
+	// subversion ACL parsed as twodimensional array
+	var $_acl = array();
+	// username
+	var $_user;
+	// the user's memberships
+	var $_groups;
+
+	function RepositoryTree($aclFile, $username) {
+		$this->_user = $username;
+		$this->_acl = $this->_parseToAcl($aclFile, $this->_user);
+		if ($this->_acl === false) {
+			exit;
+		}
+		$this->_groups = $this->_getGroupsForUser($this->_acl, $this->_user);
+	}
+	
+	/**
+	* Helper for the constructor
+	 * @return two-dimensional associative array with ACL[section][entry]
+	 */
+	function _parseToAcl($aclFile, $username) {
+		if (!file_exists($aclFile)) {
+			trigger_error("the ACL file does not exist");
+			return false;
+		}
+		$acl = parse_ini_file($aclFile, true);
+		if (count($acl) < 2) {
+			trigger_error("the ACL file must contain at least two sections, groups and a root path");
+		}
+		return $acl;
+	}
+	
+	/**
+	* Helper for the constructor
+	 * @return array with the groups that the user is member of
+	 */
+	function _getGroupsForUser($acl, $username) {
+		if (!isset($acl['groups'])) {
+			return array();
+		}
+		$groups = array();
+		$all = $acl['groups'];
+		foreach ($all as $groupname => $group) {
+			$gu = explode(',',$group);
+			foreach ($gu as $u) {
+				if (trim($u)==$username) $groups[] = $groupname;
+			}
+		}
+		return $groups;
+	}
+	
+	/**
+	 * @returns the groups that the user belongs to, in an array
+	 */
+	function getGroups() {
+		return $this->_groups;
+	}
+	
+	/**
+	 * @return an array of RepositoryEntryPoint for the user
+	 */
+	function getEntryPoints() {
 	
 	}
 
 }
 
+class RepositoryEntryPoint {
+	var $path;
+	var $readonly;
+	
+	function RepositoryEntryPoint($path) {
+		$this->path = $path;
+	}
+	
+	function setReadOnly($flag) {
+		$this->readonly = $flag;
+	}
+	
+	function isReadOnly() {
+		return $this->readonly;
+	}
+	
+	function getPath() {
+		return $this->path;
+	}
+	
+	function getDisplayname() {
+		return substr($this->path, strrpos($this->path, '/')+1);
+	}
+}
 ?>
--- www/open/tree/RepositoryTree.test.php
+++ www/open/tree/RepositoryTree.test.php
@@ -1,15 +1,71 @@
-<?
-
+<?php
 require("../../lib/simpletest/setup.php");
 
-class TestNisse extends UnitTestCase {
-	function TestOfLogging() {
+require(dirname(__FILE__)."/RepositoryTree.class.php");
+
+class TestRepositoryTree extends UnitTestCase {
+	
+	var $tempfile;
+	var $tree;
+	
+	function TestRepositoryTree() {
 		$this->UnitTestCase();
 	}
-	function testCreatingNewFile() {
-		$this->assertEqual('osse', 'bosse');
+	
+	function setUp() {
+		$this->createNewFile();
 	}
+	
+	function tearDown() {
+		unlink($this->tempfile);
+	}
+
+	function createNewFile() {
+		$this->tempfile = tempnam("/tmp", "test-repos-access");
+		$handle = fopen($this->tempfile, "w");
+		fwrite($handle, "[groups]\n");
+		fwrite($handle, "aproject =  svensson, test\n");
+		fwrite($handle, "2project = test, svensson ,bengtsson\n");
+		fwrite($handle, "3project = test, bengtsson,svensson\n");
+		fwrite($handle, "anotherproject = bengtsson, svensson2, test\n");
+		fwrite($handle, "anotherproject2 = asvensson, bsvensson\n");
+		fwrite($handle, "\n");
+		fwrite($handle, "[/]\n");
+		fwrite($handle, "\n");
+		fwrite($handle, "[/svensson]\n");
+		fwrite($handle, "svensson = r\n");
+		fwrite($handle, "\n");
+		fwrite($handle, "[/test]\n");
+		fwrite($handle, "test = rw\n");
+		fwrite($handle, "\n");
+		fwrite($handle, "[/aproject]\n");
+		fwrite($handle, "@aproject = rw\n");
+		fclose($handle);
+		
+		$this->tree = new RepositoryTree($this->tempfile, 'svensson');
+	}
+	
+	function testGroups() {
+		$groups = $this->tree->getGroups();
+		$this->assertEqual(3, count($groups));
+		$this->assertEqual('aproject', $groups[0]);
+		$this->assertEqual('2project', $groups[1]);
+		$this->assertEqual('3project', $groups[2]);
+	}
+	
+	
+	
+	
+	// test the small entry point class too
+	function testRepositoryEntryPoint() {
+		$e = new RepositoryEntryPoint('/my/own/folder');
+		$this->assertEqual('/my/own/folder', $e->getPath());
+		$this->assertEqual('folder', $e->getDisplayName());
+		$e->setReadOnly(true);
+		$this->assertEqual(true, $e->isReadOnly());
+	}
+	
 }
     
-$test = &new TestNisse();
+$test = &new TestRepositoryTree();
 $test->run(new HtmlReporter());
