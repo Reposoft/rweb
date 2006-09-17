146ae4896099cc5f66c569d8052059af682e8098
Staffan Olsson <staffan@repos.se>
2006-09-17 17:40:31 +0000

    Converted to using mavenrepo libs, manual import for those not in a public lib.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@1633 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ repos-svn-access/lib/install-to-repo.bat
@@ -0,0 +1,9 @@
+rem install svnAnt to local repository
+rem - download version 1.0 from http://subclipse.tigris.org/svnant.html
+set VERSION=1.0
+call mvn install:install-file -Dfile=svnant.jar -DgroupId=org.tigris.subclipse -DartifactId=svnant -Dversion=%VERSION% -Dpackaging=jar
+call mvn install:install-file -Dfile=svnClientAdapter.jar -DgroupId=org.tigris.subclipse -DartifactId=svnant-svnclientadapter -Dversion=%VERSION% -Dpackaging=jar
+call mvn install:install-file -Dfile=svnjavahl.jar -DgroupId=org.tigris.subclipse -DartifactId=svnant-svnjavahl -Dversion=%VERSION% -Dpackaging=jar
+
+echo Installed SvnAnt %VERSION% to local maven repository
+pause
--- repos-svn-access/uberjar.bat
+++ repos-svn-access/uberjar.bat
@@ -1,10 +1,23 @@
 @echo works better as instruction then as batch file
 
+rem now this should do it:
+call mvn assembly:assembly -Ddescriptor=src/main/assembly/with-dependencies.xml
+
+rem run tests
+@echo -
+@echo running a test case from the new jar with no other depencencies than junit
+@echo -
+set TESTCASE=se.repos.svn.checkout.simple.PersonalWorkingCopyIntegrationTest
+java -cp "C:\Documents and Settings\%USERNAME%\.m2\repository\junit\junit\4.1\junit-4.1.jar";target\repos-svn-access-1.0-SNAPSHOT-with-dependencies.jar;target\repos-svn-access-1.0-SNAPSHOT-tests.jar junit.textui.TestRunner %TESTCASE%
+
+echo fat jar built and tested, press enter to exit
+pause
+exit
+
+rem --- Old instructions, for use with 'system' scope libs.
 rem create the uberjar
 mvn assembly:assembly -Ddescriptor=src/main/assembly/with-dependencies.xml
 rem now manually unpack the jars from the 'lib' folder into the uberjar, because the assembly plugin just places them in the root of the archive
 @echo now manually unpack the jars from the 'lib' folder into the uberjar, because the assembly plugin just places them in the root of the archive
 rem make test jar
 mvn jar:test-jar
-rem run tests
-java -cp "C:\Documents and Settings\solsson\.m2\repository\junit\junit\4.1\junit-4.1.jar";target\repos-svn-access-1.0-SNAPSHOT-with-dependencies.jar;target\repos-svn-access-1.0-SNAPSHOT-tests.jar junit.textui.TestRunner se.repos.svn.checkout.simple.PersonalWorkingCopyIntegrationTest
