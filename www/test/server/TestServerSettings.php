180074ecd95825647099924bd14bf062c89664f8
ermin <ermin@94f62164-8fde-0310-83d7-e1105c8c1596>
2006-10-02 17:52:54 +0000

    Added mb_http_input test
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@1774 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/test/server/TestServerSettings.php
+++ www/test/server/TestServerSettings.php
@@ -1,25 +1,30 @@
 <?php
 
-require("../../lib/simpletest/setup.php");
+require("../../lib/simpletest/setup.php");
 
-class TestServerSettings extends UnitTestCase {
-	
-	function testMagicQuotes() {
-		$this->assertEqual(0, get_magic_quotes_gpc(), "PHP magic_quotes_gpc should be OFF");
-	}
-	
-	function testFileUploads() {
-		$this->assertEqual(1, ini_get('file_uploads'), "File uploads should be allowed");
-		$maxsize = ini_get('upload_max_filesize');
-		$M = 1048576;
-		$this->assertTrue($maxsize > 10*$M, "10 MB file uploads must be allowed, but upload_max_filesize is $maxsize.");
-	}
-	
-	function testUrlFopen() {
-		$this->assertTrue(1, ini_get('allow_url_fopen'), "allow_url_fopen must be enabled");
-	}
-}
+class TestServerSettings extends UnitTestCase {
+	
+	function testMagicQuotes() {
+		$this->assertEqual(0, get_magic_quotes_gpc(), "PHP magic_quotes_gpc should be OFF");
+	}
+	
+	function testFileUploads() {
+		$this->assertEqual(1, ini_get('file_uploads'), "File uploads should be allowed");
+		$maxsize = ini_get('upload_max_filesize');
+		$M = 1048576;
+		$this->assertTrue($maxsize > 10*$M, "10 MB file uploads must be allowed, but upload_max_filesize is $maxsize.");
+	}
+	
+	function testUrlFopen() {
+		$this->assertTrue(1, ini_get('allow_url_fopen'), "allow_url_fopen must be enabled");
+	}
 
-testrun(new TestServerSettings());
+	function testMB_HTTP_Input() {
+		$this->assertTrue(ini_get('mb_http_input()'));
+	}
+}
 
-?>
+testrun(new TestServerSettings());
+
+?>
+
