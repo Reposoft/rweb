7dbe386091966bc9da7c5b7ba50cd260becd3999
ermin <ermin@94f62164-8fde-0310-83d7-e1105c8c1596>
2006-10-02 07:34:32 +0000

    moved extraction functions to uncompress.php
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@1763 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/lib/smarty/install.php
+++ www/lib/smarty/install.php
@@ -5,9 +5,11 @@
 <PRE>
 <?PHP
 
+require '../uncompress.php';
+
 $home = "http://smarty.php.net";
 $version = "2.6.14";
-$archive = "$home/distributions/Smarty-$version";
+$archive = "$home/distributions/Smarty-$version.tar.gz";
 $file = "Smarty-$version";
 
 $basedir = dirname(__FILE__);
@@ -15,57 +17,22 @@ $dir_backslash = rtrim($basedir, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
 $dir = str_replace('\\', '/', $dir_backslash);
 
 
-uncompress($archive.".tar.gz", $dir.$file.".tar", 10000000 );
-function uncompress( $srcFileName, $dstFileName, $fileSize ){
-	// getting content of the compressed file
-	$zp = gzopen( $srcFileName, "r" );
-	$data = fread ( $zp, $fileSize );
-	gzclose( $zp );
+/*
+	extract GZ archive
+	arg 1 is an absolute path to a gz archive
+	arg 2 is the extracted file's name
+	arg 3 is optional. default value is 1 000 000 000. it has to be larger than the extracted file 
+*/
+uncompressGZ($archive, $file.".tar", 10000000);
 
-	// writing uncompressed file
-	$fp = fopen( $dstFileName, "w" );
-	fwrite( $fp, $data );
-	fclose( $fp );
-}
-
-$filename = $dir.$file.".tar";
-
-// Read in the TAR file
-$fp = fopen($filename, "rb");
-$tar_file = fread($fp, filesize($filename));
-fclose($fp);
-
-$tar_length = strlen($tar_file);
-$offset = 0;
-while($offset < $tar_length) {
-	// If we read a block of 512 nulls, we are at the end of the archive
-	if(substr($tar_file,$offset,512) == str_repeat(chr(0),512)){
-		echo "done";
-		break;
-	}
-
-	$file_name		= rtrim(substr($tar_file,$offset,100),chr(0));
-	$file_size		= octdec(substr($tar_file,$offset + 124,12));
-	$file_contents	= substr($tar_file,$offset + 512,$file_size);
-	
-	if (strpos($file_name, "libs/")) {
-		$newpath = substr($file_name, strpos($file_name, "libs/"));
-		echo $dir.$newpath."\n";
-		if ($file_size == 0 && !is_dir($dir.$newpath) && $newpath != "."){
-			mkdir($dir.$newpath);
-		} elseif ($file_size > 0) {
-			$fileFromTar = fopen($dir.$newpath,"wb");
-			fwrite($fileFromTar,$file_contents);
-			fclose($fileFromTar);
-		}
-	}
-	
-	$offset += 512 + (ceil($file_size / 512) * 512);
-}
-
-unlink($dir.$file.".tar");
-
-
+/*
+	extract TAR archive
+	arg 1 is an absolute path to a gz archive
+	arg 2 is the extracted file's name. it is optional. default value is the same path as the tar file
+	arg 3 is optional. it should be used only if a special directory from the tar file is needed.  
+*/
+uncompressTAR($file.".tar", null, "libs");
+unlink($file.".tar");  // delete the tar file
 ?> 
 </PRE>
 </BODY>
