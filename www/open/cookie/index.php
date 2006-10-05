11d4649569d440e2a8f4518e347fd896693e6c3a
Staffan Olsson <staffan@repos.se>
2006-10-05 18:52:12 +0000

    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@1834 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/open/cookie/index.php
+++ www/open/cookie/index.php
@@ -1,4 +1,7 @@
 <?php
+// return a cookie value in ajax response for browsers like firefox 1.5 that don't have cookies in xhtml+xml documents
 $name=$_GET['name'];
-echo $_COOKIE[$name];
+if (isset($_COOKIE[$name])) {
+	echo $_COOKIE[$name];
+}
 ?>
