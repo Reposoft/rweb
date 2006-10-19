c4d9131de5d4ef1dbacbad1e795ec1238856d4ef
Staffan Olsson <staffan@repos.se>
2006-10-19 20:34:09 +0000

    Some more thinking about the details plugin.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@1944 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/open/details/index.php
+++ www/open/details/index.php
@@ -2,6 +2,10 @@
 // get the details of a file and return as JSON or HTML
 require( dirname(dirname(dirname(__FILE__)))."/account/login.inc.php" );
 
+// what's the format?
+// make same json for directory listing and single file details?
+// how to use as plugin?
+
 $cmd = 'list --xml '.escapeArgument(getTargetUrl()).'';
 $return = login_svnPassthru($cmd);
 if ($return) {
