90a6094893a19e1b39a15709135a7b45ee2b072e
Staffan Olsson <staffan@repos.se>
2006-11-08 14:30:41 +0000

    upgraded to javahl 1.4.2
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@1996 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-svn-access/lib/install-to-repo.bat
+++ repos-svn-access/lib/install-to-repo.bat
@@ -3,7 +3,11 @@ rem - download version 1.0 from http://subclipse.tigris.org/svnant.html
 set VERSION=1.1.0-RC2
 call mvn install:install-file -Dfile=svnant.jar -DgroupId=org.tigris.subclipse -DartifactId=svnant -Dversion=%VERSION% -Dpackaging=jar
 call mvn install:install-file -Dfile=svnClientAdapter.jar -DgroupId=org.tigris.subclipse -DartifactId=svnant-svnclientadapter -Dversion=%VERSION% -Dpackaging=jar
-call mvn install:install-file -Dfile=svnjavahl.jar -DgroupId=org.tigris.subclipse -DartifactId=svnant-svnjavahl -Dversion=%VERSION% -Dpackaging=jar
+rem call mvn install:install-file -Dfile=svnjavahl.jar -DgroupId=org.tigris.subclipse -DartifactId=svnant-svnjavahl -Dversion=%VERSION% -Dpackaging=jar
+
+rem install the jar from the official javahl distribution
+set JAVAHL_VERSION=1.4.2
+call mvn install:install-file -Dfile=svnjavahl.jar -DgroupId=org.tigris.subversion -DartifactId=javahl -Dversion=%JAVAHL_VERSION% -Dpackaging=jar
 
 echo Installed SvnAnt %VERSION% to local maven repository
 pause
