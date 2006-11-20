befc9cbbc1c1bb4c1f33cb4c27099f831c2a3ebe
Staffan Olsson <staffan@repos.se>
2006-11-20 10:24:57 +0000

    With mbstring.internal_encoding commented out in php.ini, the explicit convert to UTF-8 works (in PHP 5.2 at least)
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2035 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/test/server/encoding/index.php
+++ www/test/server/encoding/index.php
@@ -14,6 +14,7 @@ echo(mb_strlen($var)." multibyte characters\n");
 
 echo("Detected query param encoding: ".mb_detect_encoding($var)."\n");
 // if mbstring.encoding_translation = On is enabled, both the above report same number of characters
+echo("PHP internal encoding: ".mb_internal_encoding()."\n");
 echo("PHP input encoding (for GET): ".mb_http_input("G")."\n");
 echo("PHP output encoding: ".mb_http_output()."\n");
 
