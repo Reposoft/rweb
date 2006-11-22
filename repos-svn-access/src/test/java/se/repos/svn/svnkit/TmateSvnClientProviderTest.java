b33c05d5b26c313e33d85d4d8406cbeb40dd054f
Staffan Olsson <staffan@repos.se>
2006-11-22 10:52:25 +0000

    Changed package name javasvn to svnkit
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2058 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-svn-access/src/test/java/se/repos/svn/svnkit/TmateSvnClientProviderTest.java
+++ repos-svn-access/src/test/java/se/repos/svn/svnkit/TmateSvnClientProviderTest.java
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package se.repos.svn.javasvn;
+package se.repos.svn.svnkit;
 
 import java.io.File;
 
