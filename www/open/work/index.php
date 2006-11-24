4965297b30d8b23dce0dde6e8955380dfc7022de
ermin <ermin@94f62164-8fde-0310-83d7-e1105c8c1596>
2006-11-24 08:05:24 +0000

    branch added
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2086 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/open/work/index.php
+++ www/open/work/index.php
@@ -9,10 +9,19 @@ $repository = getParent(getTargetUrl());
 // it is possible to bring up the save as dialog or a client download here, before the new page is shown
 
 $p = new Presentation();
-$p->assign('target', $target);
+
 $p->assign('downloadUrl', $downloadUrl);
 $p->assign('repository', $repository);
 
-$p->display();
+// $requestUrlArray = array('target' => $target, 'oldname' => 'test.xml', 'tofolder' => 'branches', 'newname' => 'test2.xml', 'move' => '0', 'message' => '', 'submit' => 'Copy');
+$oldname = basename($target);
+$newname = time() . '-' . getReposUser() . '-' . $oldname;
 
+$p->assign('pathToCopyForm', '../../edit/copy/');
+$p->assign('target', $target);
+$p->assign('oldname', $oldname);
+$p->assign('tofolder', '/branches');
+$p->assign('newname', $newname);
+$p->display();
+						 
 ?>
