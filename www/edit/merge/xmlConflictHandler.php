e6234eb36b0c65c4c63acf34a590f9f2545f838e
ermin <ermin@94f62164-8fde-0310-83d7-e1105c8c1596>
2006-12-02 12:19:31 +0000

    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2128 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ www/edit/merge/xmlConflictHandler.php
@@ -0,0 +1,65 @@
+<?php
+
+
+function resolveConflicts($fileContents){
+	
+}
+
+function findConflict($fileContents){
+	$conflictMarker = false;
+	foreach ($fileContents as $key=>$value){
+		if (strpos($value, '<<<<<<< .working') === 0){
+			$conflictMarker = true;
+		}
+		if (strpos($value, '>>>>>>> .merge') === 0){
+			$conflict[] = $value;
+			return $conflict;
+		}
+		if ($conflictMarker == true){
+			$conflict[] = $value;
+		}
+	}
+}
+
+
+function chooseWorking($fileContents){
+	$deleteRow = false;
+	foreach ($fileContents as $key=>$value){
+		if (strpos($value, '<<<<<<< .working') === 0){
+			unset($fileContents[$key]);
+		}
+		if (strpos($value, '=======') === 0){
+			$deleteRow = true;
+		}
+		if (strpos($value, '>>>>>>> .merge') === 0){
+			unset($fileContents[$key]);
+			$deleteRow = false;
+		}
+		if ($deleteRow == true){
+			unset($fileContents[$key]);
+		}
+	}
+	return $fileContents;
+}
+
+function chooseMerge($fileContents){
+	$deleteRow = false;
+	foreach ($fileContents as $key=>$value){
+		if (strpos($value, '<<<<<<< .working') === 0){
+			unset($fileContents[$key]);
+			$deleteRow = true;
+		}
+		if (strpos($value, '=======') === 0){
+			unset($fileContents[$key]);
+			$deleteRow = false;
+		}
+		if (strpos($value, '>>>>>>> .merge') === 0){
+			unset($fileContents[$key]);
+		}
+		if ($deleteRow == true){
+			unset($fileContents[$key]);
+		}
+	}
+	return $fileContents;
+}
+?>
