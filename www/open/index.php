1489ca96ae64dc7add31bf3cfe53e7577e32f77d
Staffan Olsson <staffan@repos.se>
2006-12-06 10:43:22 +0000

    Moved calendar to tools folder. phpicalendar should be a folder under tools/calendar/
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2149 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/open/index.php
+++ www/open/index.php
@@ -9,7 +9,7 @@ $type = substr($url, strrpos($url, '.') + 1);
 // iCalendar files
 if ($type=='ics') {
 		setcookie("repos-calendar", $url, time()+3600, '/');
-        header("Location: ".getConfig('repos_web')."/phpicalendar/");
+	header("Location: ".getWebapp()."tools/calendar/");
 } else {
         header("Location: ".getConfig('repo_root')."$url");
 }
