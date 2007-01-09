25c9298afaa483fbd0e741d9e516e0433993937d
Staffan Olsson <staffan@repos.se>
2007-01-09 15:36:32 +0000

    Creting folder only if it does not exist. Adding date to docs report.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2280 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/lib/phpdoc/makedocs.sh
+++ www/lib/phpdoc/makedocs.sh
@@ -5,9 +5,15 @@ echo Assuming that the working folder is the folder where the script is.
 cd ..
 cd ..
 REPOS_WEB=$(pwd)
-mkdir "$REPOS_WEB-docs"
+if [ ! -d "$REPOS_WEB-docs" ]
+then
+	mkdir "$REPOS_WEB-docs"
+fi
 cd lib
 cd phpdoc
-phpdoc -t "$REPOS_WEB-docs" -o HTML:frames:default -d "$REPOS_WEB" -i "$REPOS_WEB/lib/*, $REPOS_WEB/test/*, *.test.*" -ti "Repos PHP documentation" > $REPOS_WEB-docs/docs-report.txt
 
-phpdoc -t "$REPOS_WEB-docs/repos-test-docs" -o HTML:frames:default -f "$REPOS_WEB/test/*, $REPOS_WEB/*.test.*" -i "$REPOS_WEB/lib/*" -ti "Repos PHP test documentation" > $REPOS_WEB-docs/docs-test-report.txt
+echo $(date) > $REPOS_WEB-docs/docs-report.txt
+phpdoc -t "$REPOS_WEB-docs" -o HTML:frames:default -d "$REPOS_WEB" -i "$REPOS_WEB/lib/*, $REPOS_WEB/test/*, *.test.*" -ti "Repos PHP documentation" >> $REPOS_WEB-docs/docs-report.txt
+
+echo $(date) > $REPOS_WEB-docs/docs-test-report.txt
+phpdoc -t "$REPOS_WEB-docs/repos-test-docs" -o HTML:frames:default -f "$REPOS_WEB/test/*, $REPOS_WEB/*.test.*" -i "$REPOS_WEB/lib/*" -ti "Repos PHP test documentation" >> $REPOS_WEB-docs/docs-test-report.txt
