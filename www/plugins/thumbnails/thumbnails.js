69560537b84cace6d6acbe766e854ae0da80ddf2
Staffan Olsson <staffan@repos.se>
2007-01-30 09:41:04 +0000

    Thumbnail script now depends on Repos.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2445 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/plugins/thumbnails/thumbnails.js
+++ www/plugins/thumbnails/thumbnails.js
@@ -1,17 +1,29 @@
 
 $(document).ready(function() {
-	Thumbnails.init();
+	Repos.thumbnails.init();
 } );
 
-Thumbnails = new Object();
-Thumbnails.init = function() {
+Repos.thumbnails = new Object();
+
+Repos.thumbnails.init  = function() {
 	$('.file').each(function() {
 		// TODO disqualify non-images (anything that open/thumb/ can't process)
-		Thumbnails.add(this);
+		Repos.thumbnails.add(this);
 	} );
 }
 
-Thumbnails.add = function(aTag) {
+Repos.thumbnails.add = function(aTag) {
 	console.log('thumbnailing image '+aTag.id);
+	var src = aTag.getAttribute('href');
+	var p
+	// tod handle liks to open/file/ (with target already) too
 	$(aTag).append('<img src="/repos/open/thumb/?target='+aTag.getAttribute('href')+'"/>');
+}
+
+Repos.thumbnails.getTarget = function(href) {
+	return '';
+}
+
+Repos.thumbnails.getRev = function(href) {
+	return '';
 }
