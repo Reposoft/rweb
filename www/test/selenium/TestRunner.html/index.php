2d48e71f2134eb3dc42869ea4d10b2df45ae58d9
Staffan Olsson <staffan@repos.se>
2007-02-01 14:13:55 +0000

    Forward to downloaded selenium, but it does not work so well.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2491 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ www/test/selenium/TestRunner.html/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *
+ *
+ * @package
+ */
+require('../../../conf/repos.properties.php');
+
+$webapp = getWebapp();
+$q = getSelfQuery();
+
+$q = preg_replace('/(test|resultsUrl)=[.\/]*/','$1=..%2F..%2F..%2Ftest', $q);
+
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Location: {$webapp}lib/selenium/core/TestRunner.html?$q");
+
+?>
