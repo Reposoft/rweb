c2d0979d17f96ed02c833699fde1b27a577dda7f
ermin <ermin@94f62164-8fde-0310-83d7-e1105c8c1596>
2007-02-13 13:02:59 +0000

    installation of compressor added
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2565 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/lib/tinymce/install.php
+++ www/lib/tinymce/install.php
@@ -29,6 +29,33 @@ if(download($archive, $tmp)) $report->info("Download complete.");
 decompressZip($tmp, $dir);
 
 System::deleteFile($tmp);
+
+$version = "1_1_0";
+$archive = "http://prdownloads.sourceforge.net/tinymce/tinymce_compressor_php_$version.zip?download";
+$license = "http://wiki.moxiecode.com/index.php/TinyMCE:License";
+
+$basedir = dirname(__FILE__).'/tinymce/jscripts/tiny_mce';
+$dir = strtr($basedir, "\\", '/');
+$tmp = $dir.'/downloaded.tmp';
+$extracted_folder = "$dir/";
+
+if(download($archive, $tmp)) $report->info("Download complete.");
+
+decompressZip($tmp, $dir);
+
+if (!copy("$dir/tinymce_compressor_php/tiny_mce_gzip.js", "$dir/tiny_mce_gzip.js")) {
+	$report->warn("Failed to copy $dir/tinymce_compressor_php/tiny_mce_gzip.js to $dir. You should do it manually.");
+} else {
+	$report->info("Successfully copied $dir/tinymce_compressor_php/tiny_mce_gzip.js to $dir.");
+}
+
+if (!copy("$dir/tinymce_compressor_php/tiny_mce_gzip.php", "$dir/tiny_mce_gzip.php")) {
+	$report->warn("Failed to copy $dir/tinymce_compressor_php/tiny_mce_gzip.php to $dir. You should do it manually.");
+} else {
+	$report->info("Successfully copied $dir/tinymce_compressor_php/tiny_mce_gzip.php to $dir.");
+}
+
+System::deleteFile($tmp);
 $report->ok("Done.");
 $report->display();
 ?>
