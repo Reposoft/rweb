b96794e070e8fb2c56d3bf35992d4f85f939311d
ermin <ermin@94f62164-8fde-0310-83d7-e1105c8c1596>
2007-02-13 08:41:41 +0000

    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2556 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/view/index.php
+++ www/view/index.php
@@ -4,6 +4,11 @@ require(dirname(dirname(__FILE__)) . '/conf/System.class.php');
 
 if(isset($_GET['result'])) {
 	$resultFile = System::getApplicationTemp('pages') . $_GET['result'];
+	if(!file_exists($resultFile)){
+		echo "This page does not exist. <br />";
+		echo "Result pages are temporary. $resultFile has expired.";
+		exit;
+	}
 	$handle = fopen($resultFile, "r");
 	fpassthru($handle);
 	fclose($handle);
