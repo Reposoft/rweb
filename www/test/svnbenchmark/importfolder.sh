33962e6322ab420f88161235959b94a98ed36d98
Staffan Olsson <staffan@repos.se>
2007-02-14 21:10:05 +0000

    looks like all the performance problems on ARM is in svn.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2582 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ www/test/svnbenchmark/importfolder.sh
@@ -0,0 +1,14 @@
+REPO=http://test.repos.se/testrepo/test
+
+mkdir a
+
+svn import --username test --password test --non-interactive a/ $REPO/importfolder/ -m "benchmark"
+
+for F in a b c d e f g h i j k l m n o p q r s t u v x y z
+do
+	svn import --username test --password test --non-interactive a/ $REPO/importfolder/$F -m "import-$F"
+done
+rmdir a
+
+svn delete --username test --password test --non-interactive a/ $REPO/importfolder/ -m "benchmark done"
+
