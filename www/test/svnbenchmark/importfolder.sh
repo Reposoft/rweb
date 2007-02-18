c8910742f1d58a865c583eef45ee4a2abceacf20
Staffan Olsson <staffan@repos.se>
2007-02-18 19:52:48 +0000

    allow test both with file:// and http:// repository, showing that file is considerably faster on debian sarge/ARM
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2604 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/test/svnbenchmark/importfolder.sh
+++ www/test/svnbenchmark/importfolder.sh
@@ -1,14 +1,34 @@
-REPO=http://test.repos.se/testrepo/test/trunk
+
+#REPO=http://test.repos.se/testrepo/test/trunk
+
+if [ -z "$1" ]
+then
+ REPOF=/tmp/svntest
+ mkdir $REPOF
+ svnadmin create $REPOF
+ REPO="file:///$REPOF"
+else
+ REPO="$1"
+fi
+echo "Running benchmark with repo $REPO, start time $(date)"
 
 mkdir a
 
 svn import --username test --password test --non-interactive a/ $REPO/importfolder/ -m "benchmark"
 
-for F in a b c d e f g h i j k l m n o p q r s t u v x y z
+#for F in a b c d e f g h i j k l m n o p q r s t u v x y z
+for F in a b c d e f g h i j
 do
 	svn import --username test --password test --non-interactive a/ $REPO/importfolder/$F -m "import-$F"
 done
 rmdir a
 
-svn delete --username test --password test --non-interactive $REPO/importfolder -m "benchmark done"
+svn delete --username test --password test --non-interactive $REPO/importfolder/ -m "benchmark done"
+
+echo "Benchmark done, end time $(date)"
+
+if [ ! -z "$REPOF" ]
+then
+ rm -Rf "$REPOF"
+fi
 
