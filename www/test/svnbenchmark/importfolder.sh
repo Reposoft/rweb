348e0bd5b286139b792f9fb911a75aa133bed5ce
Staffan Olsson <staffan@repos.se>
2007-02-21 16:01:14 +0000

    using temp folder for import too
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2659 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/test/svnbenchmark/importfolder.sh
+++ www/test/svnbenchmark/importfolder.sh
@@ -12,16 +12,17 @@ else
 fi
 echo "Running benchmark with repo $REPO, start time $(date)"
 
-mkdir a
+TEST=/tmp/a
+mkdir $TEST
 
-svn import --username test --password test --non-interactive a/ $REPO/importfolder/ -m "benchmark"
+svn import --username test --password test --non-interactive $TEST $REPO/importfolder/ -m "benchmark"
 
 #for F in a b c d e f g h i j k l m n o p q r s t u v x y z
 for F in a b c d e f g h i j
 do
-	svn import --username test --password test --non-interactive a/ $REPO/importfolder/$F -m "import-$F"
+	svn import --username test --password test --non-interactive $TEST $REPO/importfolder/$F -m "import-$F"
 done
-rmdir a
+rmdir $TEST
 
 svn delete --username test --password test --non-interactive $REPO/importfolder/ -m "benchmark done"
 
