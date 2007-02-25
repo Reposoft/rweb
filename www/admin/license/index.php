63198f580dddb97186cba56d4177688beb385b6b
Staffan Olsson <staffan@repos.se>
2007-02-25 20:31:53 +0000

    Added a license page for installations encoded with Zend Guard.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2693 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ www/admin/license/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ *
+ * @package
+ */
+
+require('../../conf/Presentation.class.php');
+
+$p = new Presentation();
+
+if (function_exists('zend_loader_file_licensed')) {
+	$p->assign($license, zend_loader_file_licensed());
+}
+
+$p->display();
+
+?>
