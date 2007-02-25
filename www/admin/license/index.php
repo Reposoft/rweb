a6fb68bb920bb1efc9ef2813e7a9c9a6279a05cc
Staffan Olsson <staffan@repos.se>
2007-02-25 21:24:02 +0000

    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2695 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/admin/license/index.php
+++ www/admin/license/index.php
@@ -10,7 +10,7 @@ require('../../conf/Presentation.class.php');
 $p = new Presentation();
 
 if (function_exists('zend_loader_file_licensed')) {
-	$p->assign($license, zend_loader_file_licensed());
+	$p->assign('license', zend_loader_file_licensed());
 }
 
 $p->display();
