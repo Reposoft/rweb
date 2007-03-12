9ec7fbb5717891c9426b3304418d81d0ec52316e
Staffan Olsson <staffan@repos.se>
2007-03-12 14:16:16 +0000

    Added page for repository verify. Added test for host size, only navigation so far. Removed min-width for .row to solve REPOS-118.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2862 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ www/admin/svnverify/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Runs svnadmin verify for the repository
+ *
+ * @package
+ */
+ 
+require('../repos-backup.inc.php' );
+
+require(dirname(dirname(dirname(__FILE__))) . "/conf/Report.class.php" );
+$report = new Report();
+
+$repo = getConfig('local_path');
+
+$command = new Command('svnadmin');
+$command->addArgOption('verify', $repo);
+// it is unlikely that this page is called form command line, so we might as well output html
+echo('<pre>');
+$command->passthru();
+echo('</pre>');
+
+$report->display();
+
+?>
