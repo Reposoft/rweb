c6bdd45b709e8bee9c11bb72859f01f843f28468
ermin <ermin@94f62164-8fde-0310-83d7-e1105c8c1596>
2007-03-15 12:22:37 +0000

    REPOS-124 Plugin for automatic filename extension
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@2884 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ www/plugins/filename/filename.inc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *
+ *
+ * @package
+ */
+function filename_getHeadTags($webapp) {
+	return array('<script type="text/javascript" src="'.$webapp.'plugins/filename/filename.js"></script>');
+}
+
+?>
---
+++ www/plugins/filename/filename.js
@@ -0,0 +1,66 @@
+/**
+ *	Form fields with class "remember-extension" get automatically modified
+ *	if a user forgets the extension. The extension is read from the same form
+ *	field when the page is loaded.
+ */
+
+var filenamePluginExtension = '';
+
+$(document).ready(function() {
+	fileExtensionRead();
+	$('.remember-extension').change(function() {checkNewFileExtension()});
+});
+
+function fileExtensionRead() {
+	if ($('.remember-extension').val()) {
+		var localFilename = $('.remember-extension').val();
+		var i = getFileExtension(localFilename);
+		if (i != '') return i;				// if no extension is found in this field check in the .current-name field
+	}
+	if ($('.current-name').val()) {
+		var localFilename = $('.current-name').val();
+		var i = getFileExtension(localFilename);
+		if (i != '') return i;
+	}
+	return '';
+}
+
+function getFileExtension(filename) {
+	var j = filename.match(/\.(\w*$)/);
+	if (j) {
+		filenamePluginExtension = j[0];		// saves the extension only if it exists
+		return filenamePluginExtension;		// in order to prevent losing the last stored extension
+	}
+	return '';
+}
+
+function addFileExtension(newFileName) {
+	if (filenamePluginExtension) {
+		return newFileName + filenamePluginExtension;		
+	}
+	var i = fileExtensionRead();		// check the fields for extension once again
+	if (i != '') {						// and if it's found store it in filenamePluginExtension
+		filenamePluginExtension = i;
+		return newFileName + filenamePluginExtension;
+	}
+	return newFileName;					// if no extension is found return the filename without extension
+}
+
+function getDefaultExtension() {
+
+}
+/**
+ *	Check if a new file has an extension and if it doesn't add the extension
+ *	found in the form field with class "remember-extension".
+ */
+function checkNewFileExtension() {
+	if($('.remember-extension').val() != '') {
+		var newFileName = $('.remember-extension').val();
+		var i = getFileExtension(newFileName);
+		if (!i) {
+			$('.remember-extension').val(addFileExtension(newFileName));
+		} else {
+			filenamePluginExtension = i;
+		}
+	}
+}
