1d3f8998f33b77dc9bd8aca7b1da3b6f76c7b2c4
Staffan Olsson <staffan@repos.se>
2007-04-21 17:50:32 +0000

    Full repos configuration in apache (without SSL and subversoin ACL, since that is how the repos starterkit will be bundled). Copied the bubba startpage to home folder in distribution package. Tried to exclude smarty from encoding. Tried once again to chmod library folders properly, but still getting error messages.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@3109 94f62164-8fde-0310-83d7-e1105c8c1596

--- build-bubba.sh
+++ build-bubba.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # phpcoder/eaccelerator hard codes some path in templates/smarty, this is a workaround
-ant -Dsource.folder=/var/www/html/repos
+ant dev.include.test -Dsource.folder=/var/www/html/repos
