14d3756c5e061e1a138f07888efa542c3caff58c
Staffan Olsson <staffan@repos.se>
2007-04-24 06:24:21 +0000

    zend encoder running now with different licenses, but it produces 'Call to undefined function' in compiled templates.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@3118 94f62164-8fde-0310-83d7-e1105c8c1596

--- build-with-zend-licenses.sh
+++ build-with-zend-licenses.sh
@@ -2,28 +2,30 @@
 
 SVN_TAGS_URL="https://optime.repos.se/data/repos/tags/repos-"
 VERSION="1.1.3"
-SWITCH_URL="$SVN_TAGS_URL-$VERSION"
+SWITCH_URL="$SVN_TAGS_URL$VERSION"
 
 echo "Version number is $VERSION. Switch working copy to that tag."
 svn switch "$SWITCH_URL" .
 
 TARGET=dist
 
+mkdir dist
+
 # Optime server (Repos hosted) license
 ant $TARGET -Dzend.guard.product="Repos web" \
 	-Dapp.version=$VERSION \
 	-Dapp.version.build="Optime" \
-	-Dtarget.folder="dist-Optime"
+	-Dtarget.folder="dist/Optime"
 
 # Trial server
 ant $TARGET -Dzend.guard.product="Repos web trial" \
 	-Dapp.version=$VERSION \
 	-Dapp.version.build="Trial" \
-	-Dtarget.folder="dist-Trial"
+	-Dtarget.folder="dist/Trial"
 	
 # Inceptive
 ant $TARGET -Dzend.guard.product="Repos for Inceptive" \
 	-Dapp.version=$VERSION \
 	-Dapp.version.build="Inceptive" \
-	-Dtarget.folder="dist-Inceptive"
+	-Dtarget.folder="dist/Inceptive"
 	
