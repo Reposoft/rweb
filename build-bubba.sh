908ed1a2992a24106000be355bcbd21811d0ea82
Staffan Olsson <staffan@repos.se>
2007-04-26 12:50:42 +0000

    ready for 1.1 on bubba
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@3130 94f62164-8fde-0310-83d7-e1105c8c1596

--- build-bubba.sh
+++ build-bubba.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# Helper script tu build debian packages on a Bubba server
+
+# might need to remove existing repos package (it occupies the source folder below)
+#apt-get remove repos
+#apt-get clean repos
 
 # phpcoder/eaccelerator hard codes some path in templates/smarty, this is a workaround
 #ant dev.include.test -Dsource.folder=/var/www/html/repos
@@ -7,3 +12,5 @@ ant dist.debian -Dsource.folder=/var/www/html/repos
 # then test the compiled code before distributing
 rm -Rf /var/www/html/repos
 cp -R target/repos/ /var/www/html/
+
+#apt-get install repos
