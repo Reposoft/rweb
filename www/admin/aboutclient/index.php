92173eb3dbb95abe2ac422718a8f5d389ed8bb31
Staffan Olsson <staffan@repos.se>
2007-10-30 18:24:18 +0000

    Simplest possible fix for REPOS-207 in the case of the same apache instance service as SSL proxy.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@3536 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/admin/aboutclient/index.php
+++ www/admin/aboutclient/index.php
@@ -6,8 +6,23 @@
  */
 require('../../conf/Presentation.class.php'); 
 
+/**
+ * Client address is not always same as REMOTE_ADDRESS,
+ * for example if there is a local SSL proxy
+ */
+function getClientAddress() {
+	if (isset($_SERVER['HTTP_X_FORWARDED_SERVER'])) {
+		if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+			if ($_SERVER['HTTP_X_FORWARDED_SERVER']='127.0.0.1') {
+				return $_SERVER['HTTP_X_FORWARDED_FOR'];
+			}
+		}
+	}
+	return $_SERVER['REMOTE_ADDR'];
+}
+
 $p = new Presentation();
-$p->assign('address', $_SERVER['REMOTE_ADDR']); 
+$p->assign('address', getClientAddress()); 
 $p->assign('local', $_SERVER['IS_LOCAL_CLIENT'] ? true : false);
 $p->assign('admin', $_SERVER['IS_ADMIN_CLIENT'] ? true : false);
 $p->display();
