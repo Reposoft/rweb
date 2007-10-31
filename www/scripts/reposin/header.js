ca81b3159cfbf6efeceba828eb83eadf6bb957a6
Staffan Olsson <staffan@repos.se>
2007-10-31 08:09:30 +0000

    Restructured scripts so that only the API and the internals are in the scripts/ folder. This commit moves parts around, next commit will restore functionality.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@3544 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ www/scripts/reposin/header.js
@@ -0,0 +1,12 @@
+/*
+ * Repos @DEV@ javascript
+ *
+ * Copyright (c) 2007 Staffan Olsson (www.repos.se)
+ * May contain customer specific extensions.
+ *
+ * Contains jQuery - New Wave Javascript
+ * Copyright (c) 2007 John Resig (jquery.com)
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *
+ */
