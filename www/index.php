09b134d9057c12e56da3f800b5258989455fcc11
Staffan Olsson <staffan@repos.se>
2007-11-06 10:14:26 +0000

    Showing login status
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@3573 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/index.php
+++ www/index.php
@@ -7,6 +7,16 @@ require( 'conf/repos.properties.php' );
 require( 'conf/Presentation.class.php' );
 
 $p = Presentation::getInstance();
+
+// check authentication status
+$auth = array('http'=>false, 'repos'=>false);
+if (isset($_SERVER[PHP_AUTH_USER])) $auth['http'] = $_SERVER[PHP_AUTH_USER];
+if (isset($_COOKIE[USERNAME_KEY])) $auth['repos'] = $_COOKIE[USERNAME_KEY]; 
+// TODO we definitely need a login challenge to have a chance to detect http auth
+
+$p->assign('auth',$auth);
+
+// display public configuration values
 $p->assign('repository', getRepository());
 $p->assign('host', getHost());
 
