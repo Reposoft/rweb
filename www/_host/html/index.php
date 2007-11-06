5e76103907085d3af59005745c4bc59398eece07
Staffan Olsson <staffan@repos.se>
2007-11-06 12:56:25 +0000

    No need for the old default startpage when we make a nice /repos-web/. Erased tha last trace of the much disliked tooltip plugin.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@3577 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/_host/html/index.php
+++ www/_host/html/index.php
@@ -8,29 +8,33 @@
  * This script is intended to be placed in server root,
  * for logout to be effective with all URLs on the server.
  */
-define('REPOS_WEB_LOCAL', dirname(__FILE__) . '/repos/');
+
+$_webapp = '/repos-web/';
+if (isset($_SERVER['ReposWebapp'])) $_webapp = $_SERVER['ReposWebapp'];
+if (substr_count($_webapp, '/') > 3) $_webapp = substr($_webapp, strpos($_webapp,'/',8)); 
+$_path = dirname(__FILE__) . $_webapp;
 
 // disable caching in this page because it redirects
-header("Cache-Control: no-store, no-cache, must-revalidate");
-header("Cache-Control: post-check=0, pre-check=0", false);
-// HTTP/1.0 disable caching
-header("Pragma: no-cache");
+header('Cache-Control: no-cache');
+header('Pragma: no-cache'); // HTTP/1.0
 
 if (isset($_GET['logout'])) {
 	// http logout should be done from root url (or parent url to all pages that need authentication)
-	require(REPOS_WEB_LOCAL . 'account/logout/index.php');
+	require($_path.'account/logout/index.php');
 	exit;
 }
 
 if (isset($_GET['login'])) {
 	// do exactly the same thing as the repos login, but from this url
-	require(REPOS_WEB_LOCAL . 'account/login/index.php');
+	require($_path.'account/login/index.php');
 	exit;
 }
 
 // show start page
 if (file_exists(dirname(__FILE__).'/home/index.html')) {
 	header("Location: /home/");
-} else {
+} elseif (file_exists(dirname(__FILE__).'/start.html')) {
 	header("Location: /start.html");
+} else {
+	header("Location: ".$_webapp);
 }
