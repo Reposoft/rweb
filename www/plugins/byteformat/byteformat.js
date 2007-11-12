debdd36b3f98edefe6d852de9a7850bc04a99ef2
Staffan Olsson <staffan@repos.se>
2007-11-12 10:47:21 +0000

    Extracted format size from details plugin.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@3605 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ www/plugins/byteformat/byteformat.js
@@ -0,0 +1,31 @@
+
+(function($) {
+	
+	/**
+	 * Format tag text as a file size, with B, kB or MB.
+	 * The element text() must be a number.
+	 * @return true if contents were updated in all elements
+	 */
+	$.fn.byteformat = function() {
+		/**
+		 * @param {Number} b bytes
+		 * @return {String} formatted size
+		 */
+		var format = function(b) {
+			if (b < 1000) return b + ' B';
+			var f = 1.0 * b / 1024;
+			if (f < 0.995) return f.toPrecision(2) + ' kB';
+			if (f < 999.5) return f.toPrecision(3) + ' kB';
+			f = f / 1024;
+			if (f < 0.995) return f.toPrecision(2) + ' MB';
+			if (f < 99.95) return f.toPrecision(3) + ' MB';
+			return f.toFixed(0) + ' MB';
+		}
+		return this.each(function() {
+			var b = Number($(this).text());
+			if (b == Number.NaN) return false;
+			return $(this).html(format(b));				
+		});
+	};
+	
+})(jQuery);
