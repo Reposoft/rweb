e9f33c3ae16d86dab57b247f3b130b896c63c4b7
Staffan Olsson <staffan@repos.se>
2008-04-16 19:25:22 +0000

    Files that should have been added earlier
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@3792 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ repos-plugins/listrecursive/listrecursive.load.js
@@ -0,0 +1,6 @@
+
+Repos.service('index/', function() {
+		var w = Repos.getWebapp();
+		var t = encodeURI(Repos.getTarget());
+		$('#commandbar').append('<a id="listrecursive" href="'+w+'/open/list/?target='+t+'&recursive=1">list contents</a>');
+	});
---
+++ repos-plugins/refresh/refresh.load.js
@@ -0,0 +1,4 @@
+Repos.service('index/', function() {
+	$('<a id="refresh" class="command translate" href="#" onclick="window.location.reload( true )">refresh</a>')
+		.appendTo('#commandbar');
+});
