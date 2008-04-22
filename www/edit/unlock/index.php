f550d35ff8c24a366596fcaab9ab066c80fac8b2
Staffan Olsson <staffan@repos.se>
2008-04-22 14:14:18 +0000

    Fixed lock so that it is a real POST
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@3810 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/edit/unlock/index.php
+++ www/edit/unlock/index.php
@@ -7,13 +7,22 @@
 require('../../conf/Presentation.class.php');
 require('../SvnEdit.class.php');
 
-$targeturl = getTargetUrl();
-
-$template = Presentation::getInstance();
-
-$unlock = new SvnEdit('unlock');
-$unlock->addArgUrl($targeturl);
-$unlock->exec();
-displayEdit($template, getParent($targeturl));
+if ($_SERVER['REQUEST_METHOD']=='POST') {
+	$template = Presentation::getInstance();
+	$targeturl = getTargetUrl();
+	$unlock = new SvnEdit('unlock');
+	$unlock->addArgUrl($targeturl);
+	$unlock->exec();
+	displayEdit($template, getParent($targeturl));
+} else {
+	$target = getTarget();
+	$template = Presentation::getInstance();
+	$template->assign('target', $target);
+	$template->assign('repository', getRepository().getParent($target));
+	if (isset($_GET['download'])) {
+		$template->assign('download', 1);
+	}
+	$template->display();
+}
 
 ?>
