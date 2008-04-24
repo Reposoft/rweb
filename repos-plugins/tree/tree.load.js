07ca41cd64faf7d1b42e618a9c6964c46adba300
Staffan Olsson <staffan@repos.se>
2008-04-24 08:44:09 +0000

    Allow tree customization using query string. Tree integrates quite nicely into index now.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@3818 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-plugins/tree/tree.load.js
+++ repos-plugins/tree/tree.load.js
@@ -1,20 +1,19 @@
 
 function reposTreeIframe() {
+	var url = '/repos-plugins/tree/index.html';
+	url += '?menu=false';
+	url += '&frame=_top';
+	url += '&target='+encodeURI(Repos.getTarget());
 	// XHTML Transitional, not Strict
 	var tree = $('<iframe/>')
 		.attr('id','repostree')
 		.attr('name','tree')
-		.attr('width','300') // todo window size
+		.attr('width','400') // todo window size
 		.attr('height','500')
-		.prependTo('body');
-	tree.attr('src','/repos-plugins/tree/index.html');
-	//tree.css('float','left');
-	tree.css({
-		position: 'absolute',
-		top: '0px',
-		left: '0px'
-	});
-	$('body').css('margin-left','320px');
+		//float issue?//.insertAfter('#commandbar');
+		.insertAfter('h2');
+	tree.attr('src',url);
+	tree.css('float','left');
 };
 
 Repos.service('index/', function() {
