f99ddc1dfbe0d6fcdc1331c1bb39334f85a0fdcf
Staffan Olsson <staffan@repos.se>
2008-04-24 16:48:06 +0000

    Fixed script build so that external plugins can be excluded (normally they are excluded by not including them in the local plugins folder, but sometimes the plugins folder is a plain checkout)
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@3828 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ www/scripts/build-defaultexcludes.bat
@@ -0,0 +1,2 @@
+echo "Excluding plugins that are deprecated or in development"
+ant -Dplugins.excludes="refresh/**,jquery.jqUploader/**"
--- www/scripts/build-noplugins.bat
+++ www/scripts/build-noplugins.bat
@@ -1,2 +1,2 @@
-@echo "Set externals plugin folder to one with no javascripts, like ../errors"
-ant -Dplugins.external.folder=../errors
+echo "Setting externals plugin folder to one with no javascripts, like ../errors"
+ant -Dplugins.excludes="**/*"
