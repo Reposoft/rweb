1344f4e058844fddf4fbcc1b34b7de216ddd59c1
Staffan Olsson <staffan@repos.se>
2008-04-30 16:32:27 +0000

    Handle browser window resize
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@3862 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-plugins/forminputwidth/forminputwidth.load.js
+++ repos-plugins/forminputwidth/forminputwidth.load.js
@@ -4,11 +4,23 @@ $().ready(function() {
 	if (!/^edit\//.test(Repos.getService())) return;
 	// arbitrary safety margin
 	var margin = 40;
-	var autosize = function(input) {
-		var max = input.parents().filter('fieldset').width();
-		input.width(Math.max(input.width(), max - input.offset().left - margin));
+	var size = {}; // original width
+	var left = {}; // original left offset (might be needed when window is made smaller)
+	var autosize = function(input, max) {
+		var id = input.attr('id');
+		if (!id) return;
+		if (!size[id]) size[id] = input.width();
+		// we have no event for browser text size change, but with a quite big margin we're probably fine anyway
+		if (!left[id]) left[id] = input.offset().left; // left offset is not fully reliable when resizing down, so we need to keep the original as reference
+		input.width(Math.max(size[id], max - margin - Math.max(left[id],input.offset().left)));
 	};
-	$('input[type="text"], input[type="file"]').each( function() {
-		autosize($(this));
-	});
+	var run = function() {
+		var form = $('form:first');
+		var max = $('fieldset',form).width();
+		$('input[type="text"], input[type="file"]', form).each( function() {
+			autosize($(this), max);
+		});
+	};
+	run();
+	$(window).resize(run);
 });
