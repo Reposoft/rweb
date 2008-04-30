7ccb09dbffe2fb6775224a73d742cf583252b100
Staffan Olsson <staffan@repos.se>
2008-04-30 09:29:43 +0000

    Found a better solution to the IE6 utf8 target issue, which does not break the back button.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@3854 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-plugins/ie6utf8/ie6utf8.load.js
+++ repos-plugins/ie6utf8/ie6utf8.load.js
@@ -1,10 +1,27 @@
 // IE6 is buggy on back and refresh if there are non-ascii characters in query string
 // which we can not avoid when using SVNIndexXSLT
 if ($.browser.msie && $.browser.version == '6.0') {
+	/* breaks back button
 	var s = window.location.search.replace(/[\u0080-\uFFFF]+/g, function(s) {
 		return encodeURI(s);
 	});
 	if (s && s != window.location.search) window.location = window.location.pathname + s;
+	*/
+	// modify index so all targets that include the /svn/index/@path are encoded
+	Repos.service('index/', function() {
+		if (/[\u0080-\uFFFF]/.test(decodeURI(window.location.href))) { // optimization
+			// check all targets for utf8 characters
+			$('a').each(function() {
+				var a = $(this);
+				var h = a.attr('href');
+				if (!h) return;
+				var q = h.match(/([?&]target=)([^&]+)/);
+				if (!q || !q[2]) return;
+				var r = q[2].replace(/[\u0080-\uFFFF]+/g, function(s) {
+					return encodeURI(s);
+				});
+				a.attr('href',h.replace(q[0],q[1]+r));
+			});
+		}
+	});
 }
-
-// Repos.service('index/', /* could also replace target value in all links to avoid redirect, but with the service layer it won't be nessecary */);
