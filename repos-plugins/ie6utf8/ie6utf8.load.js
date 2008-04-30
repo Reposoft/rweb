f032e022d52e66451eb50563848ba845f34ee471
Staffan Olsson <staffan@repos.se>
2008-04-30 04:31:22 +0000

    Added a plugin that encodes the query string in IE6 if it contains utf8 characters.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@3849 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ repos-plugins/ie6utf8/ie6utf8.load.js
@@ -0,0 +1,8 @@
+// IE6 is buggy on back and refresh if there are non-ascii characters in query string
+// which we can not avoid when using SVNIndexXSLT
+if ($.browser.msie && $.browser.version == '6.0') {
+	var s = window.location.search.replace(/[\u0080-\uFFFF]+/g, function(s) {
+		return encodeURI(s);
+	});
+	if (s && s != window.location.search) window.location = window.location.pathname + s;
+}
