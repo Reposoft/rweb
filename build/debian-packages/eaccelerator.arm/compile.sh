3919c5100ea646663d4968540201a59e86ed8b79
Staffan Olsson <staffan@repos.se>
2008-05-18 12:24:44 +0000

    rebuild latest version for php5/etch
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@3916 94f62164-8fde-0310-83d7-e1105c8c1596

--- build/debian-packages/eaccelerator.arm/compile.sh
+++
@@ -1,6 +0,0 @@
-# download eAccelerator src and do
-phpize
-./configure --enable-eaccelerator=shared --with-php-config=/usr/bin/php-config --with-eaccelerator-userid=www-data
-make
-make install
-
--- build/debian-packages/eaccelerator.arm/prepare.sh
+++
@@ -1,6 +0,0 @@
-
-mkdir -p ./usr/lib/php4/20020429/
-cp /usr/lib/php4/20020429/eaccelerator.so ./usr/lib/php4/20020429/
-
-mkdir -p ./tmp/eaccelerator/
-chmod a+w ./tmp/eaccelerator/
