20f08d8de919f6461b3629fac1506804cc115726
Staffan Olsson <staffan@repos.se>
2009-04-21 11:33:33 +0000

    Fixed a javascript warning from the dummy console in tree's iframe
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4288 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/scripts/lib/firebug/firebugx.js
+++ www/scripts/lib/firebug/firebugx.js
@@ -4,7 +4,9 @@ if (!("console" in window) || !("firebug" in console))
     var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
     "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
 
+    if (!window.console) { // to avoid error "setting a property that has only a getter" in iframe
     window.console = {};
     for (var i = 0; i < names.length; ++i)
         window.console[names[i]] = function() {}
+    }
 }
