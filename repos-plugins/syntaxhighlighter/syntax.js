40e80c346c68c6444e24bad14eddef967723b1f9
Staffan Olsson <staffan@repos.se>
2009-04-22 16:09:45 +0000

    A script that can probably be adapted to dynamic loading of brushes
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4293 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-plugins/syntaxhighlighter/syntax.js
+++ repos-plugins/syntaxhighlighter/syntax.js
@@ -8,6 +8,8 @@ Repos.syntax = new Object();
 /* dp.SyntaxHinglighter library */
 Repos.syntax.dp = new Object();
 Repos.syntax.dp.path = 'lib/syntaxhighlighter/sh/';
+Repos.syntax.path = Repos.getWebapp() + Repos.syntax.dp.path;
+Repos.syntax.plugin = '/repos-plugins/syntaxhighlighter/';
 /* Maps classes/types to syntax types, class => brush */
 Repos.syntax.map = new Object();
 Repos.syntax.map["php"] = "php";
@@ -20,33 +22,60 @@ Repos.syntax.map["diff"] = "diff";
 Repos.syntax.map["txt"] = "wiki";
 Repos.syntax.map["accs"] = "acl";
 Repos.syntax.map["htp"] = "htp";
+// Note that SyntaxHighlighter also has aliases
+// cat www/lib/syntaxhighlighter/sh/scripts/shBrush*.js | grep ".aliases"
+// but we need to know them before they are loaded
 
-$().ready(function() { 
-	Repos.syntax.load();
+Repos.syntax.bundled = ['Css', 'Java', 'Plain', 'Sql', 'Bash', 'Delphi', 'JScript', 'Python', 'Vb', 'Cpp', 'Perl', 'Ruby', 'Xml', 'CSharp', 'Groovy', 'Php', 'Scala', 'Diff'];
+//Repos.syntax.custom = ['Diff', 'Acl', 'Wiki']; 
+Repos.syntax.custom = ['Acl', 'Wiki']; // have to use the bundled diff instead of ours or rendering fails silently
+
+$().ready(function() {
+	//$.getScript(Repos.syntax.path + 'scripts/shCore.js', function() {
+		// looks like it is not immediately available
+		//window.setTimeout(Repos.syntax.setup, 100);
+		Repos.syntax.setup();
+	//} );
 } );
 
-Repos.syntax.load = function() {
-	Repos.syntax.activate();
-};
-
-Repos.syntax.activate = function() {
-	//console.log('activating syntax highlighting');
+// sets classes on all elements that  sho
+Repos.syntax.setup = function() {
+	// configure
 	SyntaxHighlighter.config.tagName = 'textarea';
+	// modify page so SyntaxHighlighter will detect the boxes
+	// and load the required brushes
 	$('textarea[readonly]').each( function() {
 		var textarea = this;
 		//console.log('Found readonly textarea class="'+textarea.getAttribute('class')+'" name="'+textarea.name+'"');
 		for(type in Repos.syntax.map) {
 			if ($(textarea).is('.'+type)) {
 				var brush = Repos.syntax.map[type];
+				Repos.syntax.load(brush);
+				// syntaxhighlighter's notation for areas
 				textarea.setAttribute('class','brush:'+brush+'');
 				//console.log('loading syntax "'+textarea.getAttribute('class')+'" for area "'+textarea.name+'"'); 
 				break; // only one syntax per text area
 			}
 		}
 	} );
-	Repos.syntax.render();
+	// when all brushes have finished loading, highlight all
+	// (API does not support highlight single element)
+	//$().ajaxStop(function(){
+		Repos.syntax.enable();
+	//});
 };
 
-Repos.syntax.render = function() {
+Repos.syntax.load = function(brush) {
+	if (brush == 'js') brush == 'jscript'; // until we handle SyntaxHighlighter's aliases
+	for(i in Repos.syntax.bundled) {
+		if (brush == Repos.syntax.bundled[i].toLowerCase()) {
+			//$.getScript(Repos.syntax.path + 'scripts/shBrush' + Repos.syntax.bundled[i] + '.js');
+		}
+	}
+};
+
+Repos.syntax.enable = function() {
+	//console.log('Enabling all');
+	//window.setTimeout(SyntaxHighlighter.all, 500);
 	SyntaxHighlighter.all();
 };
