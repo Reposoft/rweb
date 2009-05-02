003a76fd5daf6c3914d2e9879f6a6ac4e6856b10
Staffan Olsson <staffan@repos.se>
2009-05-02 20:02:50 +0000

    Added id=logo to logo in command bar. Switching logo in arbortext customization.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4304 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-plugins/arbortext/arbortext.load.js
+++ repos-plugins/arbortext/arbortext.load.js
@@ -3,6 +3,6 @@ Repos.service('index/', function() {
 	$('.file-xml, .file-fos, .file-style, .file-dcf, .file-mcf, .file-sgm, .file-dita, .file-ditamap')
 		.css('background-image', 'url("/repos-plugins/arbortext/abx_icon.png")');
 
-		
+	$('#commandbar #logo').attr('width','198').attr('src', '/repos-plugins/arbortext/simonsoft-name.gif');
 
 });
