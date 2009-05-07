ef002b430df8f74aeccfddb87774d52eed007dd0
Staffan Olsson <staffan@repos.se>
2009-05-07 08:06:24 +0000

    Added the most recent reposweb include logic to plugins
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4325 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ repos-plugins/reposweb.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Defines the dependency to Repos Web-
+ * 
+ * Sets a constant ReposWeb with the root path for includes
+ * 
+ * @package admin
+ */
+
+define('ReposWeb', isset($_SERVER['REPOS_LOCAL_WEB']) ?
+	$_SERVER['REPOS_LOCAL_WEB']
+	: dirname(dirname(__FILE__)).'/repos-web/');
+
+?>
