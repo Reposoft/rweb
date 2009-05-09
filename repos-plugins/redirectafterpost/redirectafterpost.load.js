fba18dc3ab71df01a355da18dfbdaae268c2e4ba
Staffan Olsson <staffan@repos.se>
2009-05-09 15:24:49 +0000

    Found an issue with the redirect-after-post solution and meta refresh. Solved it with a special link id that triggers refresh through javascript with fixed delay.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4345 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-plugins/redirectafterpost/redirectafterpost.load.js
+++ repos-plugins/redirectafterpost/redirectafterpost.load.js
@@ -1,4 +1,8 @@
 // remove the Processing... output from the result page
 Repos.service('result/', function() {
 	$('.wait').hide();
+	// meta refresh added by result page will not be effective,
+	// so it has to be accompanied by an a-tag with redirect url as href
+	var redirect = $('#meta-refresh-url').text();
+	if (redirect) setTimeout('location.href = "'+redirect+'";', 2000);
 });
