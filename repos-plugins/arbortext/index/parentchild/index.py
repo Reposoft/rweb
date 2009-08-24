58ff405806fcb90c100b7914915e2c2afc971fb7
Staffan Olsson <staffan@repos.se>
2009-08-24 18:55:28 +0000

    Enabled mod_python and added an empty script where the solr proxy will be found
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4446 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ repos-plugins/arbortext/index/parentchild/index.py
@@ -0,0 +1,15 @@
+def index(target=None, rev=None):
+
+	if not target:
+		return 'Target is required'
+
+
+
+# example of sub-service
+#http://localhost/modpython/hello.py/hello
+#def hello(name=None):
+#    if name:
+#        return 'Hello, %s!' % name.capitalize()
+#    else:
+#        return 'Hello there!'	
+	
