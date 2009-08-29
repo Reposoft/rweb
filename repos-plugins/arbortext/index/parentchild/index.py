8e208af27429a0bbec2ec1c61532e6d4118783d3
Staffan Olsson <staffan@repos.se>
2009-08-29 08:22:08 +0000

    Simple proxy that forwards the solr response as text/plain
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4452 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-plugins/arbortext/index/parentchild/index.py
+++ repos-plugins/arbortext/index/parentchild/index.py
@@ -1,9 +1,37 @@
+import sys
+import os
+# http.client in python 3.1
+import httplib
+# urllib.parse in python 3.1
+import urllib
+
+
 def index(target=None, rev=None):
 
 	if not target:
 		return 'Target is required'
 
+	settings = getSettings()
+	
+	# http.client in python 3.x
+	url = settings["solrapp"] + settings["schema"] + "select/"
+	params = {'q': target, "start": 0, "rows": 10}
+	headers = {"Accept": "text/xml"}
+	c = httplib.HTTPConnection(settings["solrhost"], settings["solrport"])
+	c.request('GET', url + "?" + urllib.urlencode(params), headers=headers)
+	r1 = c.getresponse()
+	data = r1.read()
+	c.close()
+	if r1.status is not 200:
+		raise NameError("Query failed with status %d and response %s" % (r1.status, data))
+	return data;
 
+def getSettings():
+	
+	return {"solrhost": "localhost",
+		    "solrport": 8080,
+		    "solrapp": "/solr/",
+		    "schema": "parentchild/"}
 
 # example of sub-service
 #http://localhost/modpython/hello.py/hello
