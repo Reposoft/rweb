5a52b81b0458fdfa7604a898e4f0183893839e83
Staffan Olsson <staffan@repos.se>
2009-09-02 08:01:51 +0000

    Consistent ID's for custom css
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4474 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-search/repos-search.load.js
+++ repos-search/repos-search.load.js
@@ -53,16 +53,16 @@ $().ready(function() {
 
 reposSearchShow = function() {
 	// the page that includes Repos Search can provide an element with
-	// class "repossearchcontainer" to control the placement of the input box
-	var container = $('.repossearchcontainer').add('#commandbar').add('body').eq(0);
-	var box = $('<input id="searchinput" type="text" size="20" name="q"/>').css(reposSearchInputCss);
-	var form = $('<form id="searchform" action="/repos-search/"><input type="submit" style="display:none"/></form>').append(box);
+	// class "repos-search-container" to control the placement of the input box
+	var container = $('.repos-search-container').add('#commandbar').add('body').eq(0);
+	var box = $('<input id="repos-search-input" type="text" size="20" name="q"/>').css(reposSearchInputCss);
+	var form = $('<form id="repos-search-form" action="/repos-search/"><input type="submit" style="display:none"/></form>').append(box);
 	form.css(reposSearchFormCss).appendTo(container); // TODO display settings should be set in css
 	form.submit(reposSearchSubmit);
 };
 
 reposSearchClose = function() {
-	$('#searchdialog').remove();
+	$('#repos-search-dialog').remove();
 }
 
 reposSearchSubmit = function(ev) {
@@ -78,23 +78,24 @@ reposSearchSubmit = function(ev) {
 reposSearchStart = function() {
 	// create search result container
 	reposSearchClose();
-	var dialog = $('<div id="searchdialog"/>').css(reposSearchDialogCss);
+	var dialog = $('<div id="repos-search-dialog"/>').css(reposSearchDialogCss);
 	// start search request
-	var query = $('#searchinput').val();
-	var titles = $('<div id="searchtitles"/>');
+	var query = $('#repos-search-input').val();
+	var titles = $('<div id="repos-search-titles"/>');
 	reposSearchTitles(query, titles);
 	// build results layout
-	var title = $('<div class="searchdialogtitle"/>').css(reposSearchDialogTitleCss)
-		.append($('<a target="_blank" href="http://repossearch.com/" title="repossearch.com">Repos Search</a>"').css(reposSearchDialogTitleLinkCss));
-	var close = $('<div class="searchclose">close</div>').css(reposSearchCloseCss).click(reposSearchClose);
+	var title = $('<div class="repos-search-dialog-title"/>').css(reposSearchDialogTitleCss)
+		.append($('<a target="_blank" href="http://repossearch.com/" title="repossearch.com">Repos Search</a>"')
+		.attr('id', 'repos-search-dialog-title-link').css(reposSearchDialogTitleLinkCss));
+	var close = $('<div class="repos-search-close">close</div>').css(reposSearchCloseCss).click(reposSearchClose);
 	dialog.append(title);
 	title.append(close);
 	//dialog.append('<h1>Search results</h1>'); // would be better as title bar
 	$('<h2/>').text('Titles matching "' + query + '"').appendTo(dialog);
 	dialog.append(titles);
 	var fulltexth = $('<h2/>').text('Documents containing "' + query + '"').hide();
-	var fulltext = $('<div id="searchtext"/>');
-	var enablefulltext = $('<input id="enablefulltext" type="checkbox">').change(function() {
+	var fulltext = $('<div id="repos-search-fulltext"/>');
+	var enablefulltext = $('<input id="repos-search-fulltext-enable" type="checkbox">').change(function() {
 		if ($(this).is(':checked')) {
 			fulltexth.show();
 			fulltext.show();
@@ -106,13 +107,13 @@ reposSearchStart = function() {
 	});
 	$('<p/>').append(enablefulltext).append('<label for="enablefulltext"> Search contents</label>').appendTo(dialog);
 	dialog.append(fulltexth).append(fulltext);
-	titles.bind('repossearch-noresults', function() {
+	titles.bind('repos-search-noresults', function() {
 		enablefulltext.attr('checked', true).trigger('change');
 	});
-	close.clone(true).addClass("searchclosebottom").appendTo(dialog);
+	close.clone(true).addClass("repos-search-close-bottom").appendTo(dialog);
 	$('body').append(dialog);
 	// publish page wide event so extensions can get hold of search events
-	$().trigger('repossearch-started', [dialog[0], titles[0], fulltext[0]]);
+	$().trigger('repos-search-started', [dialog[0], titles[0], fulltext[0]]);
 };
 
 reposSearchTitles = function(query, resultDiv) {
@@ -154,7 +155,7 @@ reposSearchResults = function(json, resultContainer) {
 	var num = json.response.numFound;
 	if (num == 0) {
 		$('<p>No matches found</p>').appendTo(resultContainer);
-		resultContainer.trigger('repossearch-noresults');
+		resultContainer.trigger('repos-search-noresults');
 		return;
 	}
 	var list = $('<u/>').css(reposSearchListCss).appendTo(resultContainer);
@@ -162,7 +163,7 @@ reposSearchResults = function(json, resultContainer) {
 		var e = reposSearchPresentItem(json.response.docs[i]);
 		e.addClass(i % 2 ? 'even' : 'odd');
 		e.appendTo(list);
-		resultContainer.trigger('repossearch-result', [e[0]]); // event gets the element, jQuery
+		resultContainer.trigger('repos-search-result', [e[0]]); // event gets the element, not jQuery
 	}
 };
 
@@ -179,12 +180,12 @@ reposSearchPresentItem = function(json) {
 	var root = '/svn';
 	if (m[1]) {
 		root += '/' + m[1];
-		li.append('<a class="searchresultbase" href="' + root + '">' + m[1] + '</a>');
+		li.append('<a class="repos-search-resultbase" href="' + root + '">' + m[1] + '</a>');
 	}
-	li.append('<a class="searchresultpath" href="' + root + m[2] + '">' + m[2] + '</a>');
-	li.append('<a class="searchresultfile" href="' + root + m[2] + m[3] + '">' + m[3] + '</a>');
+	li.append('<a class="repos-search-resultpath" href="' + root + m[2] + '">' + m[2] + '</a>');
+	li.append('<a class="repos-search-resultfile" href="' + root + m[2] + m[3] + '">' + m[3] + '</a>');
 	if (json.title && json.title != m[3]) {
-		$('<span class="searchresulttitle">').text('  ' + json.title).appendTo(li);
+		$('<span class="repos-search-resulttitle">').text('  ' + json.title).appendTo(li);
 	}
 	return li;
 };
