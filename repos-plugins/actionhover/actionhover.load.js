4ef8371c8bf73777ad04623f077e6edb96da3c81
Staffan Olsson <staffan@repos.se>
2009-10-02 17:02:48 +0000

    Setting classes for rows in focus to allow custom css. Current css sets opacity on actions in background rows.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4506 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-plugins/actionhover/actionhover.load.js
+++ repos-plugins/actionhover/actionhover.load.js
@@ -1,16 +1,19 @@
-// customized formatting of file actions
+// Set classes for CSS that shows focus on pages showing multiple targets
+// Adds class background to file container that is not in focus
+// Adds class focus to file container on mouse over
+// not yet: Adds class focus to actions box inside file container on mouse over
 Repos.service('index/', function actionhover() {
-	var hide = function(q) { q.removeClass('hover').find('.actions').css('visibility','hidden'); };
-	var a = function() {
-		hide($('ul.index li.hover'));
-		$(this).addClass('hover').find('.actions').css('visibility','visible');
-	};
-	// show actions on mouseover and key focus
-	hide($('ul.index li').mouseover(a).focus(a));
-	$('ul.index').mouseout(function() {
-		hide($('li.hover', this));
+	var b = 'background';
+	var f = 'focus';
+	var all = $('ul.index li');
+	console.log('all', all);
+	all.addClass(b).mouseenter(function() {
+		console.log('enter', b);
+		$(this).removeClass(b).addClass(f);
+	}).mouseleave(function() {
+		$(this).removeClass(f).addClass(b);
 	});
 	// show first file to hint there are hidden actions (actions on folders are used rarely)
-	var first = $('.file:first').parent().find('.actions').css('visibility','visible'); // don't hover
-	$('ul.index').one('mouseover', function() { first.css('visibility','hidden'); })
+	var first = $('.file:first').parent().removeClass(b).addClass(f);
+	$('ul.index').one('mouseover', function() { first.removeClass(f).addClass(b); })
 });
