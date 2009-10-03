57528d612420406742f4aa7254ae4377c6621107
Staffan Olsson <staffan@repos.se>
2009-10-03 07:00:32 +0000

    Showing all actions for all rows results in a quite ugly block of buttons. This is another approach to be evaluated.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4507 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-plugins/actionhover/actionhover.load.js
+++ repos-plugins/actionhover/actionhover.load.js
@@ -3,17 +3,26 @@
 // Adds class focus to file container on mouse over
 // not yet: Adds class focus to actions box inside file container on mouse over
 Repos.service('index/', function actionhover() {
-	var b = 'background';
-	var f = 'focus';
-	var all = $('ul.index li');
-	console.log('all', all);
-	all.addClass(b).mouseenter(function() {
-		console.log('enter', b);
-		$(this).removeClass(b).addClass(f);
-	}).mouseleave(function() {
-		$(this).removeClass(f).addClass(b);
-	});
+	var cf = 'focus';
+	var cb = 'background';
+	var f, p, pp, n, nn;
+	var foc = function() {
+		f = $(this).removeClass(cb).addClass(cf);
+		p = f.prev().addClass('near' + cf);
+		pp = p.prev().addClass('nearnear' + cf);
+		n = f.next().addClass('near' + cf);
+		nn = n.next().addClass('nearnear' + cf);
+	};
+	var focus = function() {
+		f.removeClass(cf).addClass(cb);
+		p.removeClass('near' + cf);
+		pp.removeClass('nearnear' + cf);
+		n.removeClass('near' + cf);
+		nn.removeClass('nearnear' + cf);
+		foc.call(this);
+	};
+	var all = $('ul.index > li');
+	all.addClass(cb).mouseenter(focus);
 	// show first file to hint there are hidden actions (actions on folders are used rarely)
-	var first = $('.file:first').parent().removeClass(b).addClass(f);
-	$('ul.index').one('mouseover', function() { first.removeClass(f).addClass(b); })
+	foc.call($('.file:first').parent().removeClass(cb));
 });
