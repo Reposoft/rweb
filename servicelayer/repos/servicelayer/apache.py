2ee17f1feb3517bf2efdf42d9facad62a1bd0877
Staffan Olsson <staffan@repos.se>
2009-10-30 13:35:13 +0000

    This handler does not disturb normal svn operation. Implemented a sample ctypes call.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4584 94f62164-8fde-0310-83d7-e1105c8c1596

--- servicelayer/repos/servicelayer/apache.py
+++ servicelayer/repos/servicelayer/apache.py
@@ -6,22 +6,39 @@
 
 from mod_python import apache
 
-def uppercase(filter):
-    s = filter.read()
-    while s:
-        filter.write(s.upper())
-        s = filter.read()
-    if s is None:
-        filter.close()
+# the repos service
+import svn
+
+def fixuphandler(req):
+    ''' Identify service requelts and do not interfere with normal svn operation
+    
+    Subversion requests uses DAV methods (and GET of course)
+    and since 1.6 urls may contain ?[p=PEG][&r=REV]
+    
+    '''
+    # Quick way to identify servicelayer requests
+    if not req.args or req.args.find('s') == -1:
+        return apache.DECLINED
+    
+    req.handler = 'mod_python'
+    req.add_handler('PythonHandler', handler)
+    return apache.OK
 
 def handler(req):
-    req.content_type = 'text/plain'
-    req.write('im here \n')
-    return apache.OK
+    # get user
+    user_pw = req.get_basic_auth_pw()
+    user_name = req.user
     
-def fixuphandler(req):
-    req.handler = 'mod_python'
-    req.register_output_filter("UPPERCASE", uppercase)
-    req.add_output_filter("UPPERCASE")
-    req.add_handler('PythonHandler', handler)
+    req.content_type = "text/plain"
+    req.write("Hello user: %s\n" % user_name)
+    
+    req.write("URI: %s\n" % req.unparsed_uri)
+    req.write("Parse: %s\n" % (req.parsed_uri,))
+    req.write("Method is %s\n" % req.method)
+    req.write("Query string is: %s\n" % req.args)
+    
+    # try repository access
+    url = "http://localhost:8530" + req.uri 
+    req.write("debug: " + svn.test(url, user_name, user_pw))
+    
     return apache.OK
--- servicelayer/repos/servicelayer/svn.py
+++ servicelayer/repos/servicelayer/svn.py
@@ -1,5 +1,42 @@
 '''
-@author: solsson
+
 '''
 
+import csvn
+import csvn.auth
+import csvn.repos
 
+
+def test(url, username, password):
+    '''
+    Test svn access given url and credentials. Useful during development.
+    '''
+    user = csvn.auth.User(username, password)
+    session = csvn.repos.RemoteRepository(url, user)
+    # this could be the implementation for ?s=youngest
+    return "Latest revision is %d" % session.latest_revnum();
+
+
+class SvnAccess(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, repositoryRootUrl, user):
+        '''
+        Constructor
+        '''
+        self.repo = csvn.repos.RemoteRepository(repositoryRootUrl)
+        
+
+
+class SvnUser(csvn.auth.User):
+    
+    def __init__(self, username=None, password=None):
+        '''
+        Constructor
+        '''
+        
+        
+    
