cff098ad20e43f80acba92499b8a14fd5467d665
Staffan Olsson <staffan@repos.se>
2009-10-30 14:26:10 +0000

    properly constructing url and parsing query string
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4585 94f62164-8fde-0310-83d7-e1105c8c1596

--- servicelayer/repos/servicelayer/apache.py
+++ servicelayer/repos/servicelayer/apache.py
@@ -5,7 +5,7 @@
 """
 
 from mod_python import apache
-
+from urlparse import parse_qs
 # the repos service
 import svn
 
@@ -16,21 +16,43 @@ def fixuphandler(req):
     and since 1.6 urls may contain ?[p=PEG][&r=REV]
     
     '''
-    # Quick way to identify servicelayer requests
-    if not req.args or req.args.find('s') == -1:
+    # Subversion does not use post but we do
+    if req.method == 'POST':
+        return override(req)
+    # All other methods are dav (at least until we implement PUT with log message
+    if not req.method == 'GET':
         return apache.DECLINED
-    
+    if not req.args:
+        return apache.DECLINED
+    # All our get requests use s=[service], and subversions p and r never contain "s"
+    if req.args.find('s') == -1:
+        return apache.DECLINED
+    # The rest is ours
+    return override(req);
+
+def override(req):
+    ''' Override the default content handler with our servicelayer '''
     req.handler = 'mod_python'
-    req.add_handler('PythonHandler', handler)
+    req.add_handler('PythonHandler', servicelayer)
     return apache.OK
 
-def handler(req):
-    # get user
+def servicelayer(req):
+    # get user, already authenticated because this hander is invoked after auth handler
     user_pw = req.get_basic_auth_pw()
     user_name = req.user
     
+    (address, port) = req.connection.local_addr
+    url = 'http://%s:%d%s' % (address, port, req.uri)
+    
+    if req.method == 'POST':
+        req.write("Got post")
+    
+    args = parse_qs(req.args)
+    
     req.content_type = "text/plain"
     req.write("Hello user: %s\n" % user_name)
+    req.write("Service will use url: %s\n" % url)
+    req.write("s parameter is %s\n" % args['s'])
     
     req.write("URI: %s\n" % req.unparsed_uri)
     req.write("Parse: %s\n" % (req.parsed_uri,))
@@ -38,7 +60,6 @@ def handler(req):
     req.write("Query string is: %s\n" % req.args)
     
     # try repository access
-    url = "http://localhost:8530" + req.uri 
     req.write("debug: " + svn.test(url, user_name, user_pw))
     
     return apache.OK
