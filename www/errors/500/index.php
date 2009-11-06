6ca4b3e0a85e503a507b6043910ddfe3b8ac7bb4
Staffan Olsson <staffan@repos.se>
2009-11-06 12:57:16 +0000

    Error pages should not redirect
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4608 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/errors/500/index.php
+++ www/errors/500/index.php
@@ -8,7 +8,7 @@ $p = Presentation::getInstance();
 
 $url = getSelfUrl();
 
-$p->showError('
+$p->showErrorNoRedirect('
 This is a server error that could not be handled automatically, at the URL '.$url.'.
 <br />This type of error is often temporary. If it happens repeatedly, 
 contact <a href="mailto:support@repos.se">support@repos.se</a>.
