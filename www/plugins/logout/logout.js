4bfc527b1efd0cade77894eea0cee008db506d29
Staffan Olsson <staffan@repos.se>
2009-11-07 06:45:38 +0000

    Removing logout button for Safari becuase it does not clear the credentials
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4614 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/plugins/logout/logout.js
+++ www/plugins/logout/logout.js
@@ -2,8 +2,12 @@
 (function(){
 	
 	function check() {
-		if (Repos.getUser()) return;
 		var o = $('#logout');
+		if (jQuery.browser.safari) {
+			// logout logic does not work in safari so the button would just mislead users to believe they logged out
+			o.remove();
+		}
+		if (Repos.getUser()) return;
 		o.replaceWith('<span id="logout" title="Not logged in through Repos">'+o.html()+'</span>');
 	}
 	
