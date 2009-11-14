cc58e59b537208a04058a87d6df1d25c9c874fc9
Staffan Olsson <staffan@repos.se>
2009-11-14 20:14:29 +0000

    Our settings from edit.inc.php
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4654 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-plugins/tinymce/tinymce.load.js
+++ repos-plugins/tinymce/tinymce.load.js
@@ -17,26 +17,31 @@ Repos.edit = {
 // function to initialize editor on demand
 function Repos_loadTinyMce() {
 	// from Load on demand example at http://tinymce.moxiecode.com/examples/example_13.php
-   tinyMCE_GZ.init({
-      themes : "advanced",
-      plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
-      languages : "en",
-      disk_cache : true
-   }, function() {
-      tinyMCE.init({
-         mode : "textareas",
-         theme : "advanced",
-         plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
-         theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
-         theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-         theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-         theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
-         theme_advanced_toolbar_location : "top",
-         theme_advanced_toolbar_align : "left",
-         theme_advanced_statusbar_location : "bottom",
-         theme_advanced_resizing : true
-      });
-   });
+	tinyMCE_GZ.init({
+		plugins : "autosave,inlinepopups,fullscreen,save",
+		themes : "advanced",
+		languages : "en",
+		disk_cache : true,
+		debug : false
+	}, function() {
+		tinyMCE.init({
+			mode: "textareas",
+			plugins: "autosave,inlinepopups,fullscreen,save",
+			theme: "advanced",
+			theme_advanced_buttons1: "formatselect,separator,cut,copy,paste,separator,undo,redo,separator,bold,italic,underline,separator,bullist,numlist,separator,link,image,hr,separator,fullscreen",
+			theme_advanced_buttons2: "",
+			theme_advanced_buttons3: "",
+			remove_linebreaks: false,
+			forced_root_block: "p",
+			theme_advanced_blockformats: "h1, h2, h3, p",
+			theme_advanced_toolbar_location: "top",
+			theme_advanced_toolbar_align: "center",
+			convert_urls: "false",
+			entity_encoding: "raw",
+			//content_css: "/home/documents.css",
+			width: "900"
+		});
+	});
 }
 
 Repos.service('edit/text/', function() {
