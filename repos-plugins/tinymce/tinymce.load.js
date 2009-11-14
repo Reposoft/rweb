6510cde9a4fd82f7c6d61f01e1cd2ec956d339e0
Staffan Olsson <staffan@repos.se>
2009-11-14 19:38:53 +0000

    This example works if there is a tiny_mce script tag
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4652 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-plugins/tinymce/tinymce.load.js
+++ repos-plugins/tinymce/tinymce.load.js
@@ -1,17 +1,43 @@
 
-Repos.edit = {};
 
-$(document).ready( function() {
-	if (!Repos.edit.isEditPage()) return;
-	Repos.edit.enableMenu();	
-} );
+// configuration
+Repos.edit = {
+	tinyMceUrl: Repos.getWebapp() + 'lib/tinymce/tinymce/jscripts/tiny_mce/tiny_mce_gzip.js'
+	//tinyMceUrl: Repos.getWebapp() + 'lib/tinymce/tinymce/jscripts/tiny_mce/tiny_mce.js'
+};
+
+function Repos_loadTinyMce() {
+	// from Load on demand example at http://tinymce.moxiecode.com/examples/example_13.php
+   tinyMCE_GZ.init({
+      themes : "advanced",
+      plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+      languages : "en",
+      disk_cache : true
+   }, function() {
+      tinyMCE.init({
+         mode : "textareas",
+         theme : "advanced",
+         plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+         theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+         theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+         theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+         theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
+         theme_advanced_toolbar_location : "top",
+         theme_advanced_toolbar_align : "left",
+         theme_advanced_statusbar_location : "bottom",
+         theme_advanced_resizing : true
+      });
+   });
+}
+
+Repos.service('edit/text/', function() {
+	if (Repos.isTarget(/.*\.x?html/)) {
+		Repos.edit.enableMenu();
+	}
+});
 
 Repos.edit.enableMenu = function() {
-// if we are here the browser supports javascript
-
-// check if this is a new document by checking if textarea AND name field are empty.
-// show link HTML document while user is editing document as plain text and vice versa.
-	if (window.location.href.match("&type=html") == null) {
+	if (!window.location.href.match("&type=html")) {
 		if ($('#usertext').val() == "" && $('#name').val() == "") {
 			$('#commandbar').append(
 				'<span id="texteditor">Plain text</span>'
@@ -22,7 +48,7 @@ Repos.edit.enableMenu = function() {
 			);
 		}
 	} else {
-		tinyMCE.execCommand('mceAddControl', true, 'usertext');
+		Repos.edit.loadTinyMce(Repos.edit.openTinyMce);
 		if (tinyMCE.getContent('mce_editor_0') == "" && $('#name').val() == "") {
 			var htmlHref = window.location.href.replace(/&type=html/,"");
 			$('#commandbar').append(
@@ -35,13 +61,18 @@ Repos.edit.enableMenu = function() {
 		}
 	}
 
-// check if the file contains <meta name="Generator" content="Repos" />
-// and if it does, open it with TinyMCE.
+	// check if the file contains <meta name="Generator" content="Repos" />
+	// and if it does, open it with TinyMCE.
 	var filetext = $('#usertext').val();
 	var pattern = /<meta name="Generator" content="Repos"/;
 	if (filetext.search(pattern) >= 0) {
-		tinyMCE.execCommand('mceAddControl', true, 'usertext');
+		Repos.edit.loadTinyMce(Repos.edit.openTinyMce);
 	}
+};
+
+Repos.edit.loadTinyMce = function(callback) {
+	console.log('loading tinymce scripts from ', Repos.edit.tinyMceUrl);
+	Repos_loadTinyMce();
 };
 
 Repos.edit.checkTextarea = function() {
