ca38739bced4e5be138dddd9cebede4ebeafd68b
Staffan Olsson <staffan@repos.se>
2009-12-07 23:15:31 +0000

    Very simple plugin to select where Repos Search default gui will be accessible.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4685 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ repos-plugins/searchintegration/searchintegration.load.js
@@ -0,0 +1,8 @@
+
+// disable default initialization because it adds search box to every page
+ReposSearch.onready = false;
+
+// integrate search box on start page and in repository index
+Repos.service('index/', ReposSearch.init);
+Repos.service('open/start/', ReposSearch.init);
+Repos.service('home/', ReposSearch.init);
