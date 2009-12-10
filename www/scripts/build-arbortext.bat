d17d46bdd7216a29038e83f16537e98c702d9f61
Staffan Olsson <staffan@repos.se>
2009-12-10 14:55:56 +0000

    Disabled default "searchintegration" plugin because arbortext plugin has its own
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4688 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/scripts/build-arbortext.bat
+++ www/scripts/build-arbortext.bat
@@ -1,2 +1,2 @@
 echo "Excluding plugins not used in Arbortext customization"
-ant -Dplugins.excludes="refresh/**,jquery.jqUploader/**,admin*/**,linehistory/**,templates/**,tinymce/**"
+ant -Dplugins.excludes="refresh/**,jquery.jqUploader/**,admin*/**,linehistory/**,templates/**,tinymce/**,searchintegration/**"
