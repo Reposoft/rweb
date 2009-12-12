60dae31598e938df6388342c4bf5b00af9b51bae
Staffan Olsson <staffan@repos.se>
2009-12-12 12:38:48 +0000

    New init method for Repos Search
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4689 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-plugins/searchintegration/searchintegration.load.js
+++ repos-plugins/searchintegration/searchintegration.load.js
@@ -1,8 +1,8 @@
 
 // disable default initialization because it adds search box to every page
-ReposSearch.onready = false;
+ReposSearch_onready = false;
 
 // integrate search box on start page and in repository index
-Repos.service('index/', ReposSearch.init);
-Repos.service('open/start/', ReposSearch.init);
-Repos.service('home/', ReposSearch.init);
+Repos.service('index/', ReposSearch.initDefault);
+Repos.service('open/start/', ReposSearch.initDefault);
+Repos.service('home/', ReposSearch.initDefault);
