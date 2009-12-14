66128b84936525db4c794eb5aa81826591edee66
Staffan Olsson <staffan@repos.se>
2009-12-14 20:18:10 +0000

    Now base is appended only in multirepo
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4707 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-plugins/listrecursive/listrecursive.load.js
+++ repos-plugins/listrecursive/listrecursive.load.js
@@ -5,6 +5,6 @@ Repos.service('index/', function() {
 		$('#commandbar').append('<a id="listrecursive" href="'+w+'open/list/?target='+t+'&recursive=1">list&nbsp;contents</a>');
 		// explicit support for base parameter
 		// (generally in Repos it should be treated in an AOP fashion but it is not solved for this case)
-		var base = $('#base').text();
+		var base = Repos.getBase();
 		if (base) $('#listrecursive').attr('href', $('#listrecursive').attr('href') + '&base=' + base);
 	});
