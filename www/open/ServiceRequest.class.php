3dfe5b082a2afa37977a01e902e5bdb915b8368b
Staffan Olsson <staffan@repos.se>
2010-01-19 10:27:57 +0000

    Not possible to forward authentication headers if body has already started. Show real error message instead.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4718 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/open/ServiceRequest.class.php
+++ www/open/ServiceRequest.class.php
@@ -237,7 +237,7 @@ class ServiceRequest {
 		// don't want post request to be resent. Authentication should really have been taken care of when form was shown.
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') trigger_error('Client should have been authenticated before submit.');
 		// show a page in case the user cancels login
-		if (class_exists('Presentation')) {
+		if (!headers_sent() && class_exists('Presentation')) {
 			header($headers[0]);
 			header('WWW-Authenticate: '.$headers['WWW-Authenticate']);
 			// this is a bit different from SvnOpen which uses trigger_error, here we require html page
