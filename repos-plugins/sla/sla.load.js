e22c8dcdc7e49d5dd19684887828c929bab6f2f1
Staffan Olsson <staffan@repos.se>
2010-01-24 11:48:16 +0000

    Disabled the old sla links
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@4750 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-plugins/sla/sla.load.js
+++ repos-plugins/sla/sla.load.js
@@ -1,8 +1,12 @@
 // service Level Agreement
-var repos_sla_url = Repos.getWebapp()+'docs/agreement/';
-Repos.service('open/start/', function() {
-	$('<span>').attr('id','repos-sla').html(', see <a id="agreement" href="'+repos_sla_url+'" target="_blank">terms and conditions</a>').appendTo('#footer .legal');
-});
-Repos.service('account/login/', function() {
-	$('<a>').attr('id','repos-sla').addClass('command').attr('href',repos_sla_url).text('terms and conditions').appendTo('#commandbar');
-});
+(function() {
+	var repos_sla_url = Repos.getWebapp()+'docs/agreement/'; // deprecated location
+	var addToFooter = function() {
+		$('<span>').attr('id','repos-sla').html(', see <a id="agreement" href="'+repos_sla_url+'" target="_blank">terms and conditions</a>').appendTo('#footer .legal');
+	};
+	Repos.service('open/start/', addToFooter);
+	Repos.service('home/', addToFooter);
+	Repos.service('account/login/', function() {
+		$('<a>').attr('id','repos-sla').addClass('command').attr('href',repos_sla_url).text('terms and conditions').appendTo('#commandbar');
+	});
+})/*()disabled*/;
