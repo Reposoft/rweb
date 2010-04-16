fef10778b554eb01dd7cbb351cbc16945b7b65a9
Staffan Olsson <staffan@repos.se>
2010-04-16 14:25:22 +0000

    Unit tests must use _isWritable that is not cached.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@5303 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/open/SvnOpenFile.testi.php
+++ www/open/SvnOpenFile.testi.php
@@ -18,7 +18,7 @@ class TestIntegrationSvnOpenFile extends UnitTestCase {
 		setTestUser();
 		$file = new SvnOpenFile("/demoproject/trunk/public/xmlfile.xml");
 		$this->assertTrue($file->isLatestRevision());
-		$this->assertTrue($file->isWritable());
+		$this->assertTrue($file->_isWritable());
 		$this->assertTrue($file->isWriteAllow());
 		$this->assertEqual('', $file->getLockComment());
 		$this->assertTrue(is_numeric($file->getRevision()) && $file->getRevision() > 0);
@@ -43,7 +43,7 @@ class TestIntegrationSvnOpenFile extends UnitTestCase {
 	function testHeadVersionAnonymousUser() {
 		setTestUserNotLoggedIn();
 		$file = new SvnOpenFile("/demoproject/trunk/public/xmlfile.xml");
-		$this->assertFalse($file->isWritable(),
+		$this->assertFalse($file->_isWritable(),
 			"File may be writable in SVN, but if the user is logged in we don't know, and we'd rather say no. %s");
 	}
 
@@ -51,7 +51,7 @@ class TestIntegrationSvnOpenFile extends UnitTestCase {
 		setTestUser();
 		$file = new SvnOpenFile("/demoproject/trunk/readonly/index.html");
 		$this->assertTrue($file->isLatestRevision());
-		$this->assertFalse($file->isWritable(), "The file is in a readonly folder. %2");
+		$this->assertFalse($file->_isWritable(), "The file is in a readonly folder. %2");
 		//old definition//$this->assertEqual(1, $file->getRevision(), "Shouldn't have been changed since first commit. %s");
 		$this->assertEqual(1, $file->getRevisionLastChanged(), "Shouldn't have been changed since first commit. %s");
 		// mime type detection should be rewarded separate tests, but it's here
@@ -78,7 +78,7 @@ class TestIntegrationSvnOpenFile extends UnitTestCase {
 	function testGetContentsBinary() {
 		$file = new SvnOpenFile("/demoproject/trunk/public/images/a.gif");
 		$this->assertTrue($file->isLatestRevision());
-		$this->assertTrue($file->isWritable());
+		$this->assertTrue($file->_isWritable());
 		$this->assertFalse(strEnds($file->getContents(), "\n"));
 	}
 	
@@ -86,7 +86,7 @@ class TestIntegrationSvnOpenFile extends UnitTestCase {
 		setTestUser();
 		$file = new SvnOpenFile("/demoproject/trunk/Policy document.html", 1);
 		$this->assertFalse($file->isLatestRevision());
-		$this->assertFalse($file->isWritable());
+		$this->assertFalse($file->_isWritable());
 		$this->assertEqual(1, $file->getRevision());
 		$this->assertEqual(1, $file->getRevisionLastChanged());
 		$this->assertTrue(strlen($file->getContents()) > 1, substr($file->getContents(), 0, 20).'...');
@@ -117,7 +117,7 @@ class TestIntegrationSvnOpenFile extends UnitTestCase {
 		$file = new SvnOpenFile("/demoproject/trunk/public/thispathdoesnotexist.tmp");
 		$this->assertEqual(404, $file->getStatus());
 		// Why support this? // $this->assertEqual('HEAD', $file->getRevision(), "No revision can be found. %s");
-		$this->assertFalse($file->isWritable());
+		$this->assertFalse($file->_isWritable());
 	}
 	
 	function testNonexistingFile() {
@@ -126,14 +126,14 @@ class TestIntegrationSvnOpenFile extends UnitTestCase {
 		$this->assertEqual(404, $file->getStatus(), "The file was deleted in rev 2 and should be gone. %s");
 		// Why support this? // $this->assertEqual(2, $file->getRevision(),
 		//	 "Revision not found in svn, so return the value given to the constructor. %s");
-		$this->assertFalse($file->isWritable());
+		$this->assertFalse($file->_isWritable());
 	}
 	
 	function testOldVersionFileDeleted() {
 		setTestUser(); // needed until REPOS-21 is solved
 		$file = new SvnOpenFile("/demoproject/trunk/public/temp.txt", 1);
 		$this->assertFalse($file->isLatestRevision());
-		$this->assertFalse($file->isWritable());
+		$this->assertFalse($file->_isWritable());
 		$this->assertEqual(1, $file->getRevision());
 		$this->assertTrue(200, $file->getStatus(), "getStatus is not the HTTP status, but the equivalent. %s");
 		$this->assertEqual(MIMETYPE_UNKNOWN, $file->getType());
@@ -149,7 +149,7 @@ class TestIntegrationSvnOpenFile extends UnitTestCase {
 		setTestUser();
 		$file = new SvnOpenFile("/demoproject/trunk/old/empty.txt", 1);
 		$this->assertFalse($file->isLatestRevision());
-		$this->assertFalse($file->isWritable());
+		$this->assertFalse($file->_isWritable());
 		$this->assertEqual(1, $file->getRevision());
 		$this->assertEqual(0, $file->getSize());
 		$this->assertEqual(0 ,$file->getContents());
@@ -165,9 +165,9 @@ class TestIntegrationSvnOpenFile extends UnitTestCase {
 		$this->assertTrue($file->isLocked());
 		$this->assertTrue($file->isLockedBySomeoneElse());
 		$this->assertFalse($file->isLockedByThisUser());
-		//$this->assertFalse($file->isWritable(),"File is not writable when locked by someone else. %s");
+		//$this->assertFalse($file->_isWritable(),"File is not writable when locked by someone else. %s");
 		// for performance reasons we don't want to require an svn call to check writable
-		$this->assertTrue($file->isWritable(),"Even if it is locked it is still writable. %s");
+		$this->assertTrue($file->_isWritable(),"Even if it is locked it is still writable. %s");
 		// requires a user
 		$this->assertFalse($file->isWriteAllow(), "Current user not allowed to write to this file");
 		// TODO test as user svensson
@@ -219,7 +219,7 @@ class TestIntegrationSvnOpenFile extends UnitTestCase {
 		$this->assertEqual(200, $file->getStatus());
 		$this->assertTrue($file->isFolder());
 		$this->sendMessage("Note that isWritable should not generate an error in apache error log");
-		$this->assertTrue($file->isWritable());
+		$this->assertTrue($file->_isWritable());
 		$this->assertEqual('public', $file->getFilename());
 		$this->assertEqual('/demoproject/trunk/', $file->getFolderPath());
 		$this->assertEqual('dir', $file->getKind(), "%s");
@@ -240,7 +240,7 @@ class TestIntegrationSvnOpenFile extends UnitTestCase {
 		$this->assertEqual(301, $file->getStatus(), 
 			"HTTP status should be 301, not 302, for redirect to folder with slash. %s");
 		$this->assertTrue($file->isFolder(), "Should say isFolder==true when the slash is missing.");
-		$this->assertTrue($file->isWritable());
+		$this->assertTrue($file->_isWritable());
 		$this->assertFalse($file->isLocked(), "Folders are never locked, even if they contain a locked file. %s");
 		$this->assertFalse($file->isLockedBySomeoneElse(), "Folders are never locked, even if they contain a locked file. %s");
 		$this->assertFalse($file->isLockedByThisUser(), "Folders are never locked, even if they contain a locked file. %s");
@@ -251,7 +251,7 @@ class TestIntegrationSvnOpenFile extends UnitTestCase {
 		$this->assertEqual(200, $file->getStatus());
 		$this->assertTrue($file->isFolder());
 		$this->sendMessage("Note that isWritable should not generate an error in apache error log");
-		$this->assertFalse($file->isWritable());	
+		$this->assertFalse($file->_isWritable());	
 		$this->assertFalse($file->isLocked(), "Folders are never locked. %s");	
 	}
 	
@@ -260,7 +260,7 @@ class TestIntegrationSvnOpenFile extends UnitTestCase {
 		$this->assertEqual(403, $file->getStatus());
 		$this->assertTrue($file->isFolder());
 		$this->sendMessage("Note that isWritable should not generate an error in apache error log");
-		$this->assertFalse($file->isWritable());
+		$this->assertFalse($file->_isWritable());
 		$this->assertFalse($file->isLocked(), "Folders are never locked. %s");
 	}
 	
