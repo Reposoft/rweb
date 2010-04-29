9fd3f64ad2304c390d25c1b65bd4c16ec51507c5
Staffan Olsson <staffan@repos.se>
2010-04-29 06:05:10 +0000

    Implemented trim for some form fields (the rule should match the fields used for naming new stuff)
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@5326 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ repos-plugins/formtrim/formtrim.load.js
@@ -0,0 +1,85 @@
+(function() {
+	
+	var console = //window.console || 
+		{log: function() {}};
+	
+	var trim = function(str) {
+		// http://blog.stevenlevithan.com/archives/faster-trim-javascript
+		return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+	};
+	
+	/**
+	 * @param field Element
+	 * @return true if trimmed
+	 */
+	var fieldTrim = function(field) {
+		var v = $(field).val();
+		if (!v) {
+			return false;
+		}
+		var t = trim(v);
+		console.log(field,v,t);
+		if (t.length != v.length) {
+			$(field).val(t);
+			return true;
+		}
+		return false;
+	};
+	
+	var fieldGetLabel = function(field) {
+		console.log(field, 'label[for=' + field.id + ']', $('label[for=' + field.id + ']'));
+		return $('label[for=' + field.id + ']').text() || field.name;
+	};
+	
+	/**
+	 * @param form Element
+	 * @return array of fields that were trimmed
+	 */
+	var formTrim = function(form) {
+		var fields = $('input:text[name], input:text[name=newname], textarea[name=message]', form).not(':disabled');
+		var trimmed = [];
+		fields.each(function() {
+			console.log('formtrim field', this);
+			if (fieldTrim(this)) {
+				trimmed.push(this);
+			}
+		});
+		return trimmed;
+	};
+	
+	var formTrimAndInfo = function(form) {
+		var trimmed = formTrim(form);
+		if (trimmed.length) {
+			var labels = [];
+			for (var i = 0; i < trimmed.length; i++) {
+				labels.push(fieldGetLabel(trimmed[i]));
+			}
+			$().say('Removed leading and trailing whitespaces from fields: "' + 
+					labels.join('", "') + '". Submit again to accept. Change back to keep.');
+			return true;
+		}
+		return false;
+	};
+	
+	var enable = function() {
+		console.log('formtrim enabled');
+		$('form').one('submit', function(ev) {
+			try {
+				if (formTrimAndInfo(this)) {
+					ev.preventDefault();
+				}
+			} catch (e) {
+				console.log('formtrim error', this, e);
+				// abort submit
+				ev.preventDefault();
+			}
+		});
+	};
+	
+	Repos.service('edit/upload/', enable);
+	Repos.service('edit/text/', enable);
+	Repos.service('edit/mkdir/', enable);
+	Repos.service('edit/rename/', enable);
+	Repos.service('edit/copy/', enable);
+	
+})();
