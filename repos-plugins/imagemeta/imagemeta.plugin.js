cdc66ef9e2f3a546ffeb72e603b94c36e20b9b29
Staffan Olsson <staffan@repos.se>
2010-08-04 12:37:47 +0000

    Experimented with image display based on solr index. Using Galleriffic 2.0.
    Created a new js bundle, libs.js, for the shared scripts that Repos and plugins can expect to be loaded. This would make it easier to include the base package without including all plugins (maybe Repos API should be there too). On the other hand it does not force plugin pages to specify which libs they use.
    
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@5388 94f62164-8fde-0310-83d7-e1105c8c1596

---
+++ repos-plugins/imagemeta/imagemeta.plugin.js
@@ -0,0 +1,94 @@
+
+window.Repos = window.Repos || {service: function(){}};
+
+Repos.imagemeta = {};
+
+Repos.imagemeta.search = function(options) {
+
+	var repo = options.repo || Repos.getBase();
+	var queryUrl = '/solr/svnhead/select/?wt=json&indent=on';
+	queryUrl += '&fl=id,geo_lat,geo_long,description';
+	if (/\/$/.test(options.target)) {  // is folder
+		queryUrl += '&id_repo:' + repo;
+		queryUrl += '&q=folder:' + encodeURIComponent(options.target);
+	} else {
+		queryUrl += '&q=id:' + repo + '\\^' + encodeURIComponent(options.target);
+	}
+	
+	$.ajax({
+		dataType: 'json',
+		url: queryUrl,
+		success: options.success
+	});
+	
+};
+
+/**
+ * @return String decoded filename
+ */
+Repos.imagemeta.getFilename = function(solrDocId) {
+	return solrDocId.split('/').pop();
+};
+
+Repos.service('open/list/', function() {
+	
+	/**
+	 * @return jQuery row for the given id
+	 */
+	var getRow = function(solrDocId) {
+		var id = 'row:' + getFilename(solrDocId);
+		var r = document.getElementById(id);
+		if (!r) alert('error: ' + id + ' not found'); // todo support non-ascii chars
+		return $(r);
+	};
+	
+	var geotagged = [];
+	
+	var gps = function(doc, row) {
+		if (doc.geo_lat && doc.geo_long) {
+			geotagged.push(doc);
+			row.addClass('geotagged');
+		}
+	};
+	
+	var show = function(docs) {
+		for (var i = 0; i < docs.length; i++) {
+			var d = docs[i];
+			var row = getRow(d.id);
+			if (d.description) {
+				row.addClass('commented').find('a, img').attr('title', d.description);
+			}
+			gps(d, row);
+		}
+	};
+	
+	Repos.imagemeta.search({
+		target: Repos.getTarget(),
+		success: function(solr) {
+			show(solr.response.docs);
+		}
+	});
+	
+});
+
+Repos.service('open/file/', function() {
+	if (!$('body').is('.image')) return;
+
+	var show = function(docs) {
+		if (docs.length != 1) return;
+		var d = docs[0];
+		if (d.description) {
+			// TODO handle newlines
+			$('<p/>').addClass('comment').text(d.description).insertBefore('#footer');
+		}
+	};
+	
+	Repos.imagemeta.search({
+		target: Repos.getTarget(),
+		success: function(solr) {
+			show(solr.response.docs);
+		}
+	});	
+	
+});
+
---
+++ repos-plugins/imagemeta/imagemeta.plugin.test.js
@@ -0,0 +1,6 @@
+
+
+
+test('getFilename', function() {
+	equals(getFilename('repo1^/images/testJPEG.jpg'), 'testJPEG.jpg');
+});
