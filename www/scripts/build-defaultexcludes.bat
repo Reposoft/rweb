de062551e09788b011ccf589afe5be64919835a5
Staffan Olsson <staffan@repos.se>
2011-02-01 10:17:20 +0000

    Integration plugins should be excluded by default, and included on the host where the tools are installed
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@5512 94f62164-8fde-0310-83d7-e1105c8c1596

--- www/scripts/build-defaultexcludes.bat
+++ www/scripts/build-defaultexcludes.bat
@@ -1,2 +1,2 @@
-echo "Excluding plugins that are deprecated or in development"
-ant -Dplugins.excludes="refresh/**,jquery.jqUploader/**,arbortext/**,searchintegration/**,defaultaction/**,listrecursive/**"
+echo "Excluding plugins that are deprecated or in development, and integration plugins"
+ant -Dplugins.excludes="refresh/**,jquery.jqUploader/**,arbortext*/**,searchintegration/**,defaultaction/**,listrecursive/**,*-integration/**"
