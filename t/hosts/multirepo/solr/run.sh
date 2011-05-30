21a680a98a9763875fd7ea8fc6c2368981cc7486
Staffan Olsson <staffan@repos.se>
2011-05-30 08:43:00 +0000

    Easy to switch jetty and solr version. Solr home dir now resolved relative to current in workspace.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@5575 94f62164-8fde-0310-83d7-e1105c8c1596

--- t/hosts/multirepo/solr/run.sh
+++ t/hosts/multirepo/solr/run.sh
@@ -7,10 +7,10 @@ then
 #wget "http://www.eclipse.org/downloads/download.php?file=/jetty/7.0.0.v20091005/dist/jetty-distribution-7.0.0.v20091005.tar.gz&url=http://ftp.ing.umu.se/mirror/eclipse/jetty/7.0.0.v20091005/dist/jetty-distribution-7.0.0.v20091005.tar.gz&mirror_id=494"
 #tar xvzf jetty-distribution-7.0.0.v20091005.tar.gz
 #mv jetty-distribution-7.0.0.v20091005 jetty
-
-wget http://dist.codehaus.org/jetty/jetty-6.1.23/jetty-6.1.23.zip
-unzip jetty-6.1.23.zip
-mv jetty-6.1.23 jetty
+JETTY_VERSION=6.1.23
+wget http://dist.codehaus.org/jetty/jetty-$JETTY_VERSION/jetty-$JETTY_VERSION.zip
+unzip jetty-$JETTY_VERSION.zip
+mv jetty-$JETTY_VERSION jetty
 rm jetty-*
 
 fi
@@ -19,17 +19,25 @@ if [ ! -e jetty/webapps/solr.war ]
 then
 
 # install solr
-wget http://apache.dataphone.se/lucene/solr/1.4.0/apache-solr-1.4.0.tgz
-tar xvzf apache-solr-1.4.0.tgz
-mv apache-solr-1.4.0/dist/apache-solr-1.4.0.war jetty/webapps/solr.war
+SOLR_VERSION=1.4.1
+rm apache-solr-*
+wget http://apache.dataphone.se/lucene/solr/$SOLR_VERSION/apache-solr-$SOLR_VERSION.tgz
+tar xvzf apache-solr-$SOLR_VERSION.tgz
+rm jetty/webapps/cometd.war	# takes some time to extract
+mv apache-solr-$SOLR_VERSION/dist/apache-solr-$SOLR_VERSION.war jetty/webapps/solr.war
 rm -Rf apache-solr-*
 
 fi
 
-cd jetty
+pushd jetty
 
 #jetty7
 #java -jar start.jar OPTIONS=default,jsp -Djetty.port=8080 -Dsolr.solr.home=/Volumes/Encrypted/workspace/cms/solr/home/
 
-java -Djetty.port=8080 -Dsolr.solr.home=/Volumes/Encrypted/workspace/cms/solr/home/ -jar start.jar etc/jetty.xml etc/jetty-ajp.xml
+#SOLRHOME="$HOME/workspace/cms/solr/home/"
+SOLRHOME="$(pwd)/../../../../../../cms/solr/home/"	# any workspace
+#java -Djetty.port=8080 -Dsolr.solr.home=$HOME/workspace/cms/solr/home/ -jar
+java -Djetty.port=8080 -Dsolr.solr.home=$(pwd)/../../../../../../cms/solr/home/ -jar start.jar etc/jetty.xml etc/jetty-ajp.xml
+
+popd
 
