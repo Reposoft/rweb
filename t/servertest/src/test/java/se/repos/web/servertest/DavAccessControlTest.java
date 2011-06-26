f68b314bd00f2d89a2683e1525293019d9b88d03
Staffan Olsson <staffan@repos.se>
2011-06-26 14:18:35 +0000

    Fixed an issue with user area access rules when usernames are part of other usernames. I think the user area rule is good for production now.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@5593 94f62164-8fde-0310-83d7-e1105c8c1596

--- t/servertest/src/test/java/se/repos/web/servertest/DavAccessControlTest.java
+++ t/servertest/src/test/java/se/repos/web/servertest/DavAccessControlTest.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 import org.junit.Test;
 
+import se.repos.restclient.ResponseHeaders;
 import se.repos.restclient.RestAuthentication;
 import se.repos.restclient.RestClient;
 import se.repos.restclient.auth.RestAuthenticationSimple;
@@ -31,15 +32,27 @@ public class DavAccessControlTest {
 		RestClient c = getClient(null);
 		assertEquals("Should be prompted for authentication in user area",
 				401, c.head("/dav/user/").getStatus());
+		assertEquals("Should be prompted for authentication in user areas, before redirect so usernames can not be probed",
+				401, c.head("/dav/user/test").getStatus());
+		assertEquals("Should be prompted for authentication in user areas",
+				401, c.head("/dav/user/test/").getStatus());
 		
 		RestClient ctest = getClient("test", "test");
 		assertEquals("Test server up", 200, ctest.head("/").getStatus());
 		assertEquals("Should be able to access the personal folder", 
 				200, ctest.head("/dav/user/test/").getStatus());
+// not a requirement
+//		ResponseHeaders redir1 = ctest.head("/dav/user/test");
+//		assertEquals("Should be redirected when trailing slash is missing",
+//				301, redir1.getStatus());
+//		assertEquals("Should be redirect to user folder",
+//				getServer() + "/dav/user/test/", redir1.get("Location"));
 		assertEquals("Should be forbidden to access the other users' folders",
 				403, ctest.head("/dav/user/other/").getStatus());
 		assertEquals("Test username with matching substring",
 				403, ctest.head("/dav/user/test2/").getStatus());
+		assertEquals("Test username with matching suffix",
+				403, ctest.head("/dav/user/est/").getStatus());
 		
 		RestClient ctest2 = getClient("test2", "test2");
 		assertEquals("Test different user",	200, ctest2.head("/dav/user/test2/").getStatus());
