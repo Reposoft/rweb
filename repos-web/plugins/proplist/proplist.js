71880d17cc0330d4db2ee29d3dcb5cc44d9f7bf8
Staffan Olsson <staffan@repos.se>
2012-02-11 07:09:38 +0000

    A map with the property data now comes with the event. Also publishing the old events for each property, kind of deprecated but I think the performance cost is negligible.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@5868 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-web/plugins/proplist/proplist.js
+++ repos-web/plugins/proplist/proplist.js
@@ -1,7 +1,20 @@
 
 // Repos 1.4 loads the proplist by default, publishes the event for backwards compatibility
 Repos.service('details/', function() {
-	
-	
-	$(document).trigger('repos-proplist-loaded', [$('div.proplist')]);
+	var container = $('.proplist'), p, data = {};
+	$('dt, dd', container).each(function() {
+		if ($(this).is('dt')) {
+			p = $(this).text();
+		} else {
+			data[p] = (data[p] ? data[p] + '\n' : '') + $(this).text();
+		}
+	});
+	// emulate the old asynchronous events
+	$(document).trigger('repos-proplist-loaded', [container, data]);
+	for (var p in data) {  
+	    if (data.hasOwnProperty(p)) {  
+	    	$(container).trigger('repos-proplist-loaded-prop', [p, data[p]]);
+	    }
+	}
+	$(container).trigger('repos-proplist-loaded-prop-all');
 } );
