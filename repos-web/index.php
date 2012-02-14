b7d6e669b77c359f900bf0fd45d94707d0029f3e
Staffan Olsson <staffan@repos.se>
2012-02-14 14:07:03 +0000

    Once again struggling with the "home" and "start" pages, along with the possibility to disable Repos login and the fallback to Collection of Repositories when there's no ACL
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@5879 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-web/index.php
+++ repos-web/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Presents index_xy.html
+ * This is the pre-login page for anyone.
  */
 
 require( 'conf/repos.properties.php' );
@@ -13,6 +13,14 @@ $auth = array('http'=>false, 'repos'=>false);
 if (isset($_SERVER['PHP_AUTH_USER'])) $auth['http'] = $_SERVER['PHP_AUTH_USER'];
 if (isset($_COOKIE[USERNAME_KEY])) $auth['repos'] = $_COOKIE[USERNAME_KEY]; 
 // TODO we definitely need a login challenge to have a chance to detect http auth
+
+/*
+$webapp = getWebappUrl();
+$referrer = getHttpReferer();
+$selfUrl = getSelfUrl();
+$repoRoot = getRepositoryRoot();
+print_r(array('webapp'=>$webapp, 'ref'=>$referrer, 'self'=>$selfUrl, 'reporoot'=>$repoRoot, 'auth'=>$auth)); exit;
+*/
 
 $p->assign('auth',$auth);
 
