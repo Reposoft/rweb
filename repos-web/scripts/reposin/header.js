51e6bd5be4470535b1702008a29bc16a5654487f
Staffan Olsson <staffan@repos.se>
2012-02-20 19:40:54 +0000

    Updated header with info from current jquery.js
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@5886 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-web/scripts/reposin/header.js
+++ repos-web/scripts/reposin/header.js
@@ -1,17 +1,21 @@
 /*
- * Repos 1.3 javascript
+ * Repos 1.4 javascript
  *
  * Copyright (c) 2007-2009 Staffan Olsson (www.repos.se)
  * May contain customer specific extensions.
  *
- * Contains: jQuery JavaScript Library v1.3.2
+ * Contains: jQuery JavaScript Library v1.7.1
  * http://jquery.com/
  *
- * Copyright (c) 2009 John Resig
- * Dual licensed under the MIT and GPL licenses.
- * http://docs.jquery.com/License
+ * Copyright 2011, John Resig
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://jquery.org/license
  *
- * Date: 2009-02-19 17:34:21 -0500 (Thu, 19 Feb 2009)
- * Revision: 6246
+ * Includes Sizzle.js
+ * http://sizzlejs.com/
+ * Copyright 2011, The Dojo Foundation
+ * Released under the MIT, BSD, and GPL Licenses.
+ *
+ * Date: Mon Nov 21 21:11:03 2011 -0500
  */
 
