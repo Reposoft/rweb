c21365fb042c43934e7614b8ea4c072181ffecef
Staffan Olsson <staffan@repos.se>
2012-02-24 06:16:10 +0000

    Removed outdated service. Might be expected by the login plugin to get repos username cookie, but that solution will probably need rework for 1.4 anyway.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@5893 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-web/open/cookie/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// return a cookie value in ajax response for browsers like firefox 1.5 that don't have cookies in xhtml+xml documents
-$name=$_GET['name'];
-if (isset($_COOKIE[$name])) {
-	echo $_COOKIE[$name];
-}
-?>
