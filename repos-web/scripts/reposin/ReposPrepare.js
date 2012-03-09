69116ca8352e42018c38a9b5aadbba59e87cee57
Staffan Olsson <staffan@repos.se>
2012-03-09 11:43:19 +0000

    Compression using Closure Compiler by default. Now using distributed minimized jQuery. Compression including jQuery actually worked (with our development plugin that causes an error excluded) but there were lots of argument type warnings from jQuery code, and we might be able to do performance optimizations based on the ubiquity of jQuery by replacing bundled with hosted, etc. jQuery UI is now prebuilt too, but I think we should include it because closure compiler removes unused code and our strategy must be - if we chose to go with jQuery UI - to allow plugins to use any widget.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@5941 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-web/scripts/reposin/ReposPrepare.js
+++ repos-web/scripts/reposin/ReposPrepare.js
@@ -6,9 +6,9 @@
 /**
  * Change createElement of default 'document' instance so that all
  * libraries can work with xhtml+xml documents (most are not tested for this)
- */
 if (document.documentElement && document.documentElement.namespaceURI && document.createElementNS) {
 	document.createElement = function(t) {
 		return document.createElementNS(document.documentElement.namespaceURI, t);
 	};
 }
+ */
--- repos-web/scripts/reposin/header.js
+++ repos-web/scripts/reposin/header.js
@@ -1,21 +1,6 @@
 /*
- * Repos 1.4 javascript
- *
- * Copyright (c) 2007-2009 Staffan Olsson (www.repos.se)
- * May contain customer specific extensions.
- *
- * Contains: jQuery JavaScript Library v1.7.1
- * http://jquery.com/
- *
- * Copyright 2011, John Resig
- * Dual licensed under the MIT or GPL Version 2 licenses.
- * http://jquery.org/license
- *
- * Includes Sizzle.js
- * http://sizzlejs.com/
- * Copyright 2011, The Dojo Foundation
- * Released under the MIT, BSD, and GPL Licenses.
- *
- * Date: Mon Nov 21 21:11:03 2011 -0500
+ * Repos Web 1.4 javascript
+ * Copyright (c) 2007-2012 www.repos.se
+ * 
+ * Bundle including custom site-specific extensions.
  */
-
