aa2c538b5085f25b748d9ff52a0aa24adfb35b87
Staffan Olsson <staffan@repos.se>
2012-03-09 12:15:02 +0000

    Closure is now default build
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@5945 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-web/scripts/build-arbortext.bat
+++ repos-web/scripts/build-arbortext.bat
@@ -1,2 +1,2 @@
 echo "Excluding plugins that are not in the standard package"
-ant compress.closure -Dplugins.excludes="refresh/**,jquery.jqUploader/**,admin*/**,linehistory/**,templates/**,tinymce/**,searchintegration/**,loginconventional/**,sla/**,listrecursive/**,thumblist/**"
+ant -Dplugins.excludes="refresh/**,jquery.jqUploader/**,admin*/**,linehistory/**,templates/**,tinymce/**,searchintegration/**,loginconventional/**,sla/**,listrecursive/**,thumblist/**"
--- repos-web/scripts/build-defaultexcludes.bat
+++ repos-web/scripts/build-defaultexcludes.bat
@@ -1,2 +1,2 @@
 echo "Excluding plugins that are deprecated or in development"
-ant compress.closure -Dplugins.excludes="refresh/**,jquery.jqUploader/**,arbortext*/**,searchintegration/**,defaultaction/**,listrecursive/**"
+ant -Dplugins.excludes="refresh/**,jquery.jqUploader/**,arbortext*/**,searchintegration/**,defaultaction/**,listrecursive/**"
