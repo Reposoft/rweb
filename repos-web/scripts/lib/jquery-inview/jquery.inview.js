e35988fb44b5649a3ad05dc493e435cf118acf30
Staffan Olsson <staffan@repos.se>
2012-03-09 11:49:13 +0000

    Inview plugin now part of core. Will be needed for good thumbnailing support.
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@5942 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-web/scripts/lib/jquery-inview/jquery.inview.js
+++ repos-web/scripts/lib/jquery-inview/jquery.inview.js
@@ -1,5 +1,4 @@
 /**
- * https://raw.github.com/protonet/jquery.inview/master/jquery.inview.js
  * author Christopher Blum
  *    - based on the idea of Remy Sharp, http://remysharp.com/2009/01/26/element-in-view-event-plugin/
  *    - forked from http://github.com/zuk/jquery.inview/
