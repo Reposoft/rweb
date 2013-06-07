baa9aa5c08ad475122d9673122e7f42f16f60673
Staffan Olsson <staffan@repos.se>
2013-06-07 06:07:33 +0000

    Now ignoring console.debug in IE with developer tools, previously produced exception for missing property
    git-svn-id: https://optime.repos.se/svn/code/repos/trunk@6151 94f62164-8fde-0310-83d7-e1105c8c1596

--- repos-web/scripts/lib/firebug/firebugx.js
+++ repos-web/scripts/lib/firebug/firebugx.js
@@ -1,12 +1,13 @@
-
-if (!("console" in window) || !("firebug" in console))
-{
-    var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
-    "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
-
-    if (!window.console) { // to avoid error "setting a property that has only a getter" in iframe
-    window.console = {};
-    for (var i = 0; i < names.length; ++i)
-        window.console[names[i]] = function() {}
-    }
+if (!window.console) {
+	window.console = {};
+}
+if (!window.console.firebug) {
+	var names = [ "log", "debug", "info", "warn", "error", "assert", "dir",
+		"dirxml", "group", "groupEnd", "time", "timeEnd", "count", "trace",
+		"profile", "profileEnd" ];
+	for ( var i = 0; i < names.length; ++i) {
+		if (!window.console[names[i]]) {
+			window.console[names[i]] = function() {}
+		}
+	}
 }
