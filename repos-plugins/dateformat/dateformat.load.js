670bad79c8ebe10cafa8cd9eb7c7c9f1743d560e
Staffan Olsson <staffan@repos.se>
2014-10-13 13:14:35 +0200

    Revert duplicate code for date formatting because I might have a better idea now.
    This reverts commit 230d13c9d066bd7b8c4290f816db826100067771.

--- repos-plugins/dateformat/dateformat.load.js
+++ repos-plugins/dateformat/dateformat.load.js
@@ -95,55 +95,6 @@ Date.prototype.toISO8601String = function (format, offset) {
     if (format > 3) { str += offset; }
     return str;
 };
-
-Date.prototype.toUserString = function (format, offset) {
-    /* accepted values for the format [1-6]:
-     1 Year:
-       YYYY (eg 1997)
-     2 Year and month:
-       YYYY-MM (eg 1997-07)
-     3 Complete date:
-       YYYY-MM-DD (eg 1997-07-16)
-     4 Complete date plus hours and minutes:
-       YYYY-MM-DDThh:mmTZD (eg 1997-07-16T19:20+01:00)
-     5 Complete date plus hours, minutes and seconds:
-       YYYY-MM-DDThh:mm:ssTZD (eg 1997-07-16T19:20:30+01:00)
-     6 Complete date plus hours, minutes, seconds and a decimal
-       fraction of a second
-       YYYY-MM-DDThh:mm:ss.sTZD (eg 1997-07-16T19:20:30.45+01:00)
-    */
-    if (typeof format == 'undefined') {
-    	var format = 5;
-    }
-    if (typeof offset == 'undefined') {
-        
-        var date = this;
-    } else {
-        var d = offset.match(/([-+])([0-9]{2}):([0-9]{2})/);
-        var offsetnum = (Number(d[2]) * 60) + Number(d[3]);
-        offsetnum *= ((d[1] == '-') ? -1 : 1);
-        var date = new Date(Number(Number(this) + (offsetnum * 60000)));
-    }
-
-    var zeropad = function (num) { return ((num < 10) ? '0' : '') + num; };
-
-    var str = "";
-    str += date.getUTCFullYear();
-    if (format > 1) { str += "-" + zeropad(date.getUTCMonth() + 1); }
-    if (format > 2) { str += "-" + zeropad(date.getUTCDate()); }
-    if (format > 3) {
-        str += " " + zeropad(date.getHours()) +
-               ":" + zeropad(date.getUTCMinutes());
-    }
-    if (format > 5) {
-        var secs = Number(date.getUTCSeconds() + "." +
-                   ((date.getUTCMilliseconds() < 100) ? '0' : '') +
-                   zeropad(date.getUTCMilliseconds()));
-        str += ":" + zeropad(secs);
-    } else if (format > 4) { str += ":" + zeropad(date.getUTCSeconds()); }
-
-    return str;
-};
  
 /**
  * Dateformat jQuery plugin.
@@ -195,7 +146,7 @@ $.fn.dateformat = function(options) {
 		if ($(texttag).is('.dateformatted')) return;
 		var date = F.parse(texttag);
 		if (!date) return false;
-		$(texttag).text(date.toUserString())
+		$(texttag).text(date.toLocaleString())
 			.addClass('dateformatted')
 			.trigger('repos-dateformat-done', [date]);
 	};
