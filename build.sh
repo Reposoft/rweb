21388650c3756e6de1bdcfd96dcb60f805fc2e87
Staffan Olsson <staffan@repos.se>
2014-10-14 09:13:26 +0200

    Maintaining the Ant file just isn't worth the effort when all we want to do is to package Repos Web from a linux command line.

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+VERSION=1.5.1
+rm target -Rf
+mkdir target
+cp -r repos-plugins/ repos-web/ target/
+php target/repos-web/lib/smarty/install.php
+php target/repos-web/lib/syntaxhighlighter/install.php
+pushd target/repos-web/scripts/ && ./build-arbortext.bat && popd
+pushd target && tar cfz ../ReposWeb-$VERSION.tgz repos-web/ repos-plugins/ && popd
+
