8c6d812b518386ab9a353133d6ab00dd10243fa4
Staffan Olsson <staffan@repos.se>
2014-10-14 13:39:09 +0200

    Avoid dead end on 410 page by exposing the hostname Repos already uses everywhere.

--- repos-web/errors/410/index.php
+++ repos-web/errors/410/index.php
@@ -11,10 +11,15 @@ $url = getSelfUrl();
 
 // help CMS users detect a common cause of this error
 $enforceHostMessage = '';
-$serverName = $_SERVER['SERVER_NAME'];
+$configHost = getHost();
 $httpHost = $_SERVER['HTTP_HOST'];
-if (true || /* can we trust SERVER_NAME to be a configured one? */$serverName != $httpHost) {
+if (true) {
 	$enforceHostMessage = 'Could be that the address used for the connection is unexpected, for example an IP-number or a name without a domain.';
+}
+// this host information is already available in page meta tag repos-repository so we can expose it more clearly
+if ($configHost && !strEnds($configHost, $httpHost)) {
+	$root = asLink($configHost);
+	$enforceHostMessage .= " Try the configured name <a href=\"$url/?rweb=start\">$root</a>";
 }
 
 $p->showErrorNoRedirect('The requested resource is not available. ' . $enforceHostMessage,
