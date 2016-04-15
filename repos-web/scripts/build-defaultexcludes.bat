16dedeb5442911d2ad1fcfbbfd6854788d3114d7
Staffan Olsson <staffan@repos.se>
2016-04-15 11:18:02 +0200

    cms plugin set has become the de facto default excludes

--- repos-web/scripts/build-defaultexcludes.bat
+++ repos-web/scripts/build-defaultexcludes.bat
@@ -1,2 +1,2 @@
-echo "Excluding plugins that are deprecated or in development"
-ant -Dplugins.excludes="refresh/**,jquery.jqUploader/**,arbortext*/**,searchintegration/**,defaultaction/**,listrecursive/**"
+echo "Excluding plugins that are not in the standard package"
+ant -Dplugins.excludes="refresh/**,jquery.jqUploader/**,admin*/**,linehistory/**,templates/**,tinymce/**,searchintegration/**,loginconventional/**,sla/**,listrecursive/**,thumblist/**"
--- repos-web/scripts/build-noadmin.bat
+++
@@ -1,2 +0,0 @@
-echo "Excluding plugins that are deprecated or in development. Also excluding repos-admin plugins."
-ant -Dplugins.excludes="refresh/**,jquery.jqUploader/**,arbortext*/**,searchintegration/**,defaultaction/**,listrecursive/**,admin*/**"
