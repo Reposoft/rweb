368d8435c3d2d8874d8729a7cc4448d7356b3e3c
Staffan Olsson <staffan@repos.se>
2016-06-21 21:40:22 +0200

    Fail build if webpack finds a compile error

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cp -r repos-plugins target/
 cp -r repos-web target/
 php target/repos-web/lib/smarty/install.php
 php target/repos-web/lib/simpletest/install.php
-cd target/repos-plugins/highlight/ && npm install && ./node_modules/.bin/webpack && cd ../../../
+cd target/repos-plugins/highlight/ && npm install && ./node_modules/.bin/webpack --bail && cd ../../../
 cd target/repos-web/scripts/ && ./build-defaultexcludes.bat && cd ../../../
 cd target && tar cfz ../ReposWeb-$VERSION.tgz repos-web/ repos-plugins/ && cd ..
 cd target && zip -qr ../ReposWeb-$VERSION.zip repos-web/ repos-plugins/ && cd ..
