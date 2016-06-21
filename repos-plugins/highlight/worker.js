58866822c9f8d68bd7682090b01d53f1d24ce06e
Staffan Olsson <staffan@repos.se>
2016-06-21 21:55:34 +0200

    Load lib outside onmessage

--- repos-plugins/highlight/worker.js
+++ repos-plugins/highlight/worker.js
@@ -1,8 +1,9 @@
 
 require("css!highlight.js/styles/default.css");
 
+var highlight = require('highlight.js');
+
 onmessage = function(event) {
-  var highlight = require('highlight.js');
   console.log('In highlight worker. Content:', event.data.substring(0,20), '...');
   var result = highlight.highlightAuto(event.data);
   console.log('Highlighted: ', result.value.substring(0,20), '...')
