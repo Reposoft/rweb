984c9d4f45fabe207a570a9172bb9a050309103c
Staffan Olsson <staffan@repos.se>
2020-06-28 10:22:39 +0200

    Reproducible builds

--- build.sh
+++ build.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-VERSION=1.7.9
-rm -Rf target
-mkdir target
-cp -r repos-plugins target/
-cp -r repos-web target/
-php target/repos-web/lib/smarty/install.php
-php target/repos-web/lib/simpletest/install.php
-cd target/repos-plugins/highlight/ && npm install && ./node_modules/.bin/webpack --bail && rm -Rf node_modules && cd ../../../
-cd target/repos-web/scripts/ && ./build-defaultexcludes.bat && cd ../../../
-cd target && tar cfz ../ReposWeb-$VERSION.tgz repos-web/ repos-plugins/ && cd ..
-cd target && zip -qr ../ReposWeb-$VERSION.zip repos-web/ repos-plugins/ && cd ..
+echo "Run something like: RELEASE_VERSION= docker-compose up --build releaser"
